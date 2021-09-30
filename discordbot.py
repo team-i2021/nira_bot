ef36939179f10517acc4a9ad989a7d4ad65746cf
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2021-09-30 16:09:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -57,6 +57,9 @@ async def on_message(message):
             await message.channel.send('は、恥ずかしい....かな....\nえっ...あっ....私じゃない....？')
         return
     if re.search(r'(?:rt!|りつたん|りつ)', message.content):
+        if message.content == "rt!help":
+            await message.channel.send('さ、出番だよりつたん！\nえ、上から目線なのが気になるの...？\n...で、出番ですよりつさま！")
+            return
         rnd = random.randint(1, 4)
         if rnd == 1:
             await message.channel.send('りつたん...会ってみたいな...')
