910304c3a23a6fef6078106dd272d5488bbf7a04
nattyan-tv <undirect_sbv@yahoo.co.jp>
2021-09-30 09:43:53 +0900

    master

--- discordbot.py
+++ discordbot.py
@@ -21,13 +21,11 @@ async def on_ready():
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    global memo
-    memo = ""
     # 下のやつミスると「v0.9.2　～故に彼は猫だった～」の時みたいに猫爆弾が起爆するにゃ(botのメッセージは無視する)
     if message.author.bot:
         return
     # 猫系のワードに反応するにゃ
-    if re.search(r'(?:nyanko|neko|cat|cats|猫|ねこ|ネコ|にゃんこ|ニャンコ|NYANKO|NEKO|CAT|CATS|にゃん|にゃー|にゃ～)', message.content):
+    if re.search(r'(?:nyanko|neko|cat|cats|猫|ねこ|ネコ|にゃんこ|ニャンコ|NYANKO|NEKO|CAT|CATS|にゃん|にゃー|にゃ～|キャット|キャッツ)', message.content):
         neko_rnd = random.randint(1, 3)
         if neko_rnd == 1:
             await message.channel.send('にゃ、にゃーん？')
@@ -36,20 +34,19 @@ async def on_message(message):
         elif neko_rnd == 3:
             await message.channel.send('ごろにゃーん！')
         return
-    if re.search(r'(?:めも |メモ |memo )', message.content):
-        mes_cnt = message.content
-        memo = mes_cnt.split(" ", 2)[1]
-        await message.channel.send('記録しました')
+    # 犬系のワードにも反応するにゃ...わん！
+    if re.search(r'(?:wanko|inu|dog|dogs|wanwan|犬|いぬ|イヌ|わんこ|ワンコ|WANKO|INU|DOG|DOGS|いっぬ|わんわん|ワンワン|ドッグ|ドッグス)', message.content):
+        if re.search(r'(?:bungo stray dogs|文豪ストレイドッグス)', message.content):
+            await message.channel.send('わんわ...って文ストじゃん！（犬だと思った）')
+            return
+        inu_rnd = random.randint(1, 3)
+        if inu_rnd == 1:
+            await message.channel.send('わ、わーん？')
+        elif inu_rnd == 2:
+            await message.channel.send('いぬだわーん？')
+        elif inu_rnd == 3:
+            await message.channel.send('わんわんお！')
         return
-    if re.search(r'(?:めも　|メモ　|memo　)', message.content):
-        mes_cnt = message.content
-        memo = mes_cnt.split("　", 2)[1]
-        await message.channel.send('記録しました')
-        return
-    if message.content == "メモ" or message.content == "めも" or message.content == "memo":
-        await message.channel.send(memo)
-        return
-    return
 
 
 # Botの起動とDiscordサーバーへの接続
