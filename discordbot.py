e8bd03e3534790aa037543f8d27c549e790bcee4
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2021-10-03 01:05:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,8 +13,6 @@ async def on_ready():
     print('正常に起動しました')
     print('ニラbot v0.9.2')
     print('～故に彼女は猫だった～')
-    await client.change_presence(activity=discord.Game(name="現在起動準備中...", type=2))
-    time.sleep(10)
     await client.change_presence(activity=discord.Game(name="ニラの栽培ゲーム", type=1))
 
 # メッセージ受信時に動作する処理
@@ -23,7 +21,7 @@ async def on_message(message):
     # 下のやつミスると「v0.9.2　～故に彼は猫だった～」の時みたいに猫爆弾が起爆するにゃ(botのメッセージは無視する)
     if message.author.bot:
         return
-    if re.search(r'(?:にら|ニラ|nira|garlic)', message.content) == True and message.channel.id == "885493801878179890" or message.channel.id == "882182974559387698":
+    if re.search(r'(?:にら|ニラ|nira|garlic)', message.content) == True and message.channel.id == "885493801878179890":
         await message.channel.send('https://media.discordapp.net/attachments/885154539769036831/893843337130426398/image0.png')
         return
 
