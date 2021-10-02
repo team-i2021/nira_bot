7a8c2ec66314dbcebda7dbe14e402ef61e2feaeb
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2021-10-03 01:55:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ import discord
 from os import getenv
 import re
 import time
+import random
 
 TOKEN = getenv('DISCORD_BOT_TOKEN')
 
@@ -21,8 +22,28 @@ async def on_message(message):
     # 下のやつミスると「v0.9.2　～故に彼は猫だった～」の時みたいに猫爆弾が起爆するにゃ(botのメッセージは無視する)
     if message.author.bot:
         return
-    if re.search(r'(?:にら|ニラ|nira|garlic)', message.content):
-        await message.channel.send('https://media.discordapp.net/attachments/885154539769036831/893843337130426398/image0.png')
+    if re.search(r'(?:にら|ニラ|nira|garlic|韮|Chinese chives|Allium tuberosum)', message.content):
+        if re.search(r'(?:栽培|さいばい|サイバイ)', message.content):
+            if re.search(r'(?:水|みず|ミズ)', message.content):
+                await message.channel.send('https://nattyan-tv.github.io/tensei_disko/nira_mizu.jpg')
+                return
+            await message.channel.send('https://nattyan-tv.github.io/tensei_disko/nira_sand.jpeg')
+            return
+        if re.search(r'(?:魚|さかな|fish|サカナ)', message.content):
+            await message.channel.send('https://nattyan-tv.github.io/tensei_disko/nira_fish.png')
+            return
+        rnd = random.randint(1, 3)
+        if rnd == 1
+            await message.channel.send('https://nattyan-tv.github.io/tensei_disko/nira_a.jpg')
+            return
+        elif rnd == 2
+            await message.channel.send('https://nattyan-tv.github.io/tensei_disko/nira_b.jpg')
+            return
+        elif rnd == 3
+            await message.channel.send('https://nattyan-tv.github.io/tensei_disko/nira_c.png')
+            return
+    if re.search(r'(?:煮裸族)', message.content):
+        await message.channel.send('https://nattyan-tv.github.io/tensei_disko/nira_zoku.mp4')
         return
 
 
