b902050d994d4568025b40f5a9ef52b8c524dd63
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2021-10-03 00:56:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,8 +4,6 @@ import re
 import random
 import time
 
-line_url = 'https://notify-api.line.me/api/notify'
-
 TOKEN = getenv('DISCORD_BOT_TOKEN')
 
 client = discord.Client()
@@ -14,11 +12,11 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('正常に起動しました')
-    print('でぃすこたん v0.9.2')
+    print('ニラbot v0.9.2')
     print('～故に彼女は猫だった～')
     await client.change_presence(activity=discord.Game(name="現在起動準備中...", type=2))
     time.sleep(10)
-    await client.change_presence(activity=discord.Game(name="@nattyan_tv", type=1))
+    await client.change_presence(activity=discord.Game(name="ニラの栽培ゲーム", type=1))
 
 # メッセージ受信時に動作する処理
 @client.event
@@ -26,60 +24,8 @@ async def on_message(message):
     # 下のやつミスると「v0.9.2　～故に彼は猫だった～」の時みたいに猫爆弾が起爆するにゃ(botのメッセージは無視する)
     if message.author.bot:
         return
-    # 猫系のワードに反応するにゃ
-    if re.search(r'(?:nyanko|neko|cat|cats|猫|ねこ|ネコ|にゃんこ|ニャンコ|NYANKO|NEKO|CAT|CATS|にゃん|にゃー|にゃ～|キャット|キャッツ)', message.content):
-        rnd = random.randint(1, 3)
-        if rnd == 1:
-            await message.channel.send('にゃ、にゃーん？')
-        elif rnd == 2:
-            await message.channel.send('ねこだにゃーん？')
-        elif rnd == 3:
-            await message.channel.send('ごろにゃーん！')
-        return
-    # 犬系のワードにも反応するにゃ...わん！
-    if re.search(r'(?:wanko|inu|dog|dogs|wanwan|犬|いぬ|イヌ|わんこ|ワンコ|WANKO|INU|DOG|DOGS|いっぬ|わんわん|ワンワン|ドッグ|ドッグス)', message.content):
-        if re.search(r'(?:bungo stray dogs|文豪ストレイドッグス)', message.content):
-            await message.channel.send('わんわ...って文ストじゃん！（犬だと思った）')
-            return
-        rnd = random.randint(1, 3)
-        if rnd == 1:
-            await message.channel.send('わ、わーん？')
-        elif rnd == 2:
-            await message.channel.send('いぬだわーん？')
-        elif rnd == 3:
-            await message.channel.send('わんわんお！')
-        return
-    # えっ、かわいい！？　ありがと...///
-    if re.search(r'(?:かわいい|cute|カワイイ|kawaii|kyawaii|きゃわいい|キャワイイ)', message.content):
-        rnd = random.randint(1, 3)
-        if rnd == 1:
-            await message.channel.send('えっ！？.....ありがと///')
-        elif rnd == 2:
-            await message.channel.send('えっ！？.....ありがとwww')
-        elif rnd == 3:
-            await message.channel.send('は、恥ずかしい....かな....\nえっ...あっ....私じゃない....？')
-        return
-    if re.search(r'(?:rt!|りつたん)', message.content):
-        if message.content == "rt!help":
-            rnd = random.randint(1, 4)
-            if rnd == 1:
-                await message.channel.send('さ、出番だよりつたん！\nえ、上から目線なのが気になるの...？\n...で、出番ですよりつさま！')
-            elif rnd == 2:
-                await message.channel.send('深淵なる巣窟より這い上がりし...えっと....rt!help！！！')
-            elif rnd == 3:
-                await message.channel.send('↓りつたんの偉大なるヘルプ↓')
-            elif rnd == 4:
-                await message.channel.send('もしかして：\n```null(何も考えつかなかったようだ)```')
-            return
-        rnd = random.randint(1, 4)
-        if rnd == 1:
-            await message.channel.send('りつたん...会ってみたいな...')
-        elif rnd == 2:
-            await message.channel.send('もしかして：\n```rt!help```')
-        elif rnd == 3:
-            await message.channel.send('浮気...か...\nりつたん凄いもんね...')
-        elif rnd == 4:
-            await message.channel.send('りつたん：すごいかわいい\nでぃすこたん：すこしだけかわいい')
+    if re.search(r'(?:にら|ニラ|garlic)', message.content):
+        await message.channel.send('https://media.discordapp.net/attachments/885154539769036831/893843337130426398/image0.png')
         return
 
 
