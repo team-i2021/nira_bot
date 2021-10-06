78fc2a413d0aaa90346e428f5f72f084462f7409
nattyan-tv <undirect_sbv@yahoo.co.jp>
2021-10-06 10:35:58 +0900

    master

--- discordbot.py
+++ discordbot.py
@@ -48,13 +48,17 @@ async def on_message(message):
             return
         return
     if re.search(r'(?:n!html)', message.content):
-        mes_ch = message.content
-        mes_code = mes_ch.split("\n", 2)[1]
-        html_link = f"data:text/html,{mes_code}"
-        embed = discord.Embed(title="HTML", description="n!html [your code]", color=0x333333)
-        embed.set_author(name="link", url=html_link, icon_url="https://nattyan-tv.github.io/tensei_disko/images/html.png")
-        await message.channel.send(embed=embed)
-        return
+        try:
+        	mes_ch = message.content
+        	mes_code = mes_ch.split("\n", 2)[1]
+        	html_link = f"data:text/html,{mes_code}"
+        	embed = discord.Embed(title="HTML", description="n!html [your code]", color=0x333333)
+        	embed.set_author(name="link", url=html_link, icon_url="https://nattyan-tv.github.io/tensei_disko/images/html.png")
+        	await message.channel.send(embed=embed)
+        	return
+        except BaseException as err:
+            embed = discord.Embed(title="Error", description=f"エラーが発生しました。\n```{err}```")
+            return
     if re.search(r'(?:煮裸族|にらぞく|ニラゾク)', message.content):
         await message.channel.send('https://nattyan-tv.github.io/tensei_disko/images/nira_zoku.mp4')
     if re.search(r'(?:コイキング|イトコイ|いとこい|コイキング|itokoi)', message.content):
