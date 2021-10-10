545201a36038c11b5f5880324f6e161140a21918
nattyan-tv <undirect_sbv@yahoo.co.jp>
2021-10-10 14:05:40 +0900

    [fix]discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -322,7 +322,7 @@ async def on_message(message):
         embed = discord.Embed(title="ARK: Survival Evolved", description="[Launch ARK(Steam)](https://nattyan-tv.github.io/tensei_disko/html/launch_ark_steam.html)", color=0x555555)
         sended_mes = await message.reply(embed=embed)
     if re.search(r'(?:かなしい|つらい|ぴえん|:pleading_face:|:cry:|:sob:|:weary:|:smiling_face_with_tear:|辛|悲しい|ピエン|泣く|泣きそう|いやだ|かわいそうに|可哀そうに)', message.content):
-        sended_mes = await message.reply("https://nattyan-tv.github.io/tenesi_disko/images/kawaisou.png")
+        sended_mes = await message.reply("https://nattyan-tv.github.io/tensei_disko/images/kawaisou.png")
     if sended_mes != "":
         await sended_mes.add_reaction("<:trash:896021635470082048>")
         await asyncio.sleep(2)
