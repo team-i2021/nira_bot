04114a80dad6db879fb5f1cdeecb2c2fc037b18f
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2022-02-17 12:42:11 +0900

    slash commandに対応させるテストの一つ

--- nira.py
+++ nira.py
@@ -37,9 +37,13 @@ intents.presences = True # Presence Intentだよ
 intents.members = True # メンバーに関する情報を受け取る
 bot = commands.Bot(command_prefix="n!", intents=intents, help_command=None)
 bot.remove_command("help") #意味あるのかしらんけどjishakuのヘルプコマンド削除
+bot.load_extension("jishaku")
+
+# オーナー設定
+async def is_owner(author):
+    return author.id in n_fc.py_admin
+bot.is_owner = is_owner
 print("BOTの設定完了")
-#じしゃく
-# bot.add_cog("jishaku")
 
 
 
@@ -48,7 +52,6 @@ print("BOTの設定完了")
 #設定読み込み
 if os.path.isfile(f'{sys.path[0]}/setting.json') == False:
     exit()
-
 setting = json.load(open(f'{sys.path[0]}/setting.json', 'r'))
 home_dir = os.path.dirname(__file__)
 token = setting["tokens"]["nira_bot"]
@@ -68,62 +71,14 @@ logger = logging.getLogger(__name__)
 logger.addFilter(NoTokenLogFilter())
 formatter = '%(asctime)s$%(filename)s$%(lineno)d$%(funcName)s$%(levelname)s:%(message)s'
 logging.basicConfig(format=formatter, filename=f'{home_dir}/nira.log', level=logging.INFO)
+print("外部設定完了")
 
-bot.load_extension("jishaku")
 
-print("Loggingの設定完了")
-#オーナー設定
-async def is_owner(author):
-    return author.id in n_fc.py_admin
-bot.is_owner = is_owner
-
-print("全設定完了")
-
-bot.load_extension("cogs.not_ready")
-
-print("Cog:not_ready読み込み完了")
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=nextcord.Game(name="起動中... 1/3", type=1), status=nextcord.Status.idle)
-    func_error_count = 0
-    nira_f_num = len(os.listdir(home_dir))
-    system_list = os.listdir(home_dir)
-    logging.info((nira_f_num,system_list))
-    for i in range(nira_f_num):
-        logging.info(f"StartProcess:{system_list[i]}")
-        if system_list[i][-5:] != ".nira":
-            logging.info(f"Skip:{system_list[i]}")
-            continue
-        try:
-            cog_name = system_list[i][:-5]
-            with open(f'{home_dir}/{system_list[i]}', 'rb') as f:
-                exec(f"n_fc.{cog_name} = pickle.load(f)")
-            logging.info(f"変数[{system_list[i]}]のファイル読み込みに成功しました。")
-            if system_list[i] == "notify_token.nira":
-                logging.info("LINE NotifyのTOKENのため、表示はされません。")
-            else:
-                exec(f"logging.info(n_fc.{cog_name})")
-        except BaseException as err:
-            logging.info(f"変数[{system_list[i]}]のファイル読み込みに失敗しました。\n{err}")
-            func_error_count = 1
-    if func_error_count > 0:
-        main_content = {
-            "username": "エラーが発生しました",
-            "content": f"BOTを起動時にエラーが発生しました。変数の読み込みエラーです。\nエラーコード:`{func_error_count}`\nlogを確認してください。"
-        }
-        requests.post(main_channel, main_content)
-    print("変数の読み込み完了")
     await bot.change_presence(activity=nextcord.Game(name="起動中... 2/3", type=1), status=nextcord.Status.idle)
-    # bot.add_application_command(ping)
-    # bot.add_application_command(cog)
-    # bot.add_application_command(func)
-    # bot.add_application_command(reload)
-    bot.add_application_command(line)
-    bot.add_application_command(line_del)
-    #cogのロード
-    bot.unload_extension("cogs.not_ready")
-    print("Cogの読み込み完了")
     await bot.change_presence(activity=nextcord.Game(name="起動中... 3/3", type=1), status=nextcord.Status.idle)
     for i in range(len(list(n_fc.force_ss_list.keys()))):
         print(list(n_fc.force_ss_list.keys())[i])
@@ -205,25 +160,56 @@ async def line_del(interaction: Interaction):
             pickle.dump(n_fc.notify_token, f)
         await interaction.response.send_message(f"{interaction.channel.name}でのLINEトークンを削除しました。", ephemeral = True)
 
+func_error_count = 0
+nira_f_num = len(os.listdir(home_dir))
+system_list = os.listdir(home_dir)
+logging.info((nira_f_num,system_list))
+for i in range(nira_f_num):
+    logging.info(f"StartProcess:{system_list[i]}")
+    if system_list[i][-5:] != ".nira":
+        logging.info(f"Skip:{system_list[i]}")
+        continue
+    try:
+        cog_name = system_list[i][:-5]
+        with open(f'{home_dir}/{system_list[i]}', 'rb') as f:
+            exec(f"n_fc.{cog_name} = pickle.load(f)")
+        logging.info(f"変数[{system_list[i]}]のファイル読み込みに成功しました。")
+        if system_list[i] == "notify_token.nira":
+            logging.info("LINE NotifyのTOKENのため、表示はされません。")
+        else:
+            exec(f"logging.info(n_fc.{cog_name})")
+    except BaseException as err:
+        logging.info(f"変数[{system_list[i]}]のファイル読み込みに失敗しました。\n{err}")
+        func_error_count = 1
+if func_error_count > 0:
+    main_content = {
+        "username": "エラーが発生しました",
+        "content": f"BOTを起動時にエラーが発生しました。変数の読み込みエラーです。\nエラーコード:`{func_error_count}`\nlogを確認してください。"
+    }
+    requests.post(main_channel, main_content)
+print("変数の読み込み完了")
+
+
+try:
+    cogs_dir = home_dir + "/cogs"
+    cogs_num = len(os.listdir(cogs_dir))
+    cogs_list = os.listdir(cogs_dir)
+    for i in range(cogs_num):
+        if cogs_list[i][-3:] != ".py" or cogs_list[i] == "__pycache__" or cogs_list[i] == "not_ready.py":
+            continue
+        bot.load_extension(f"cogs.{cogs_list[i][:-3]}")
+except BaseException as err:
+    print(err)
+    main_content = {
+        "username": "エラーが発生しました",
+        "content": f"BOTを起動時にエラーが発生しました。Cogの読み込みエラーです。\n```{err}```\nサービスは終了します。"
+    }
+    requests.post(main_channel, main_content)
+    sys.exit(1)
+print("Cogの読み込み完了")
+
 
 def main():
-    try:
-        cogs_dir = home_dir + "/cogs"
-        cogs_num = len(os.listdir(cogs_dir))
-        cogs_list = os.listdir(cogs_dir)
-        for i in range(cogs_num):
-            if cogs_list[i][-3:] != ".py" or cogs_list[i] == "__pycache__" or cogs_list[i] == "not_ready.py":
-                continue
-            bot.load_extension(f"cogs.{cogs_list[i][:-3]}")
-    except BaseException as err:
-        print(err)
-        main_content = {
-            "username": "エラーが発生しました",
-            "content": f"BOTを起動時にエラーが発生しました。Cogの読み込みエラーです。\n```{err}```\nサービスは終了します。"
-        }
-        requests.post(main_channel, main_content)
-        sys.exit(1)
-    
     # BOT起動
     print("BOT起動開始...")
     bot.run(token)
