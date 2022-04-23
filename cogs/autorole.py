3bfbba2400bb2ce695a90663715d8e7c08f87e9b
nattyan-tv <undirect_sbv@yahoo.co.jp>
2022-04-23 17:33:39 +0900

    自動ロールをOFFにする機能を追加

--- cogs/autorole.py
+++ cogs/autorole.py
@@ -43,31 +43,50 @@ class autorole(commands.Cog):
     """)
     async def autorole(self, ctx: commands.Context):
         if not admin_check.admin_check(ctx.guild, ctx.author):
-            await ctx.reply("・Autoroleコマンド\nエラー：権限がありません。")
+            await ctx.reply("自動ロール\nエラー：権限がありません。")
             return
-        args = ctx.message.content.split(" ", 2)
-        if len(args) != 2:
-            await ctx.reply(f"・Autoroleコマンド\nエラー：構文が間違っています。\n書き方：`n!autorole [ロール名またはロールID]`")
+        args = ctx.message.content.split(" ", 3)
+        if len(args) == 1:
+            if ctx.guild.id in n_fc.autorole:
+                role = ctx.guild.get_role(n_fc.autorole[ctx.guild.id])
+                embed = nextcord.Embed(title="自動ロール", description=f"自動ロールを有効にしています。\n自動ロールするロールは{role.mention}です。\n`n!autorole on [ロール名又はロールID]`/`n!autorole off`", color=0x00ff00)
+            else:
+                embed = nextcord.Embed(title="自動ロール", description="自動ロールを有効にしていません。\n`n!autorole on [ロール名又はロールID]`/`n!autorole off`", color=0x00ff00)
+            await ctx.reply(embed=embed)
             return
-        role_id = None
-        try:
-            role_id = int(args[1])
-        except ValueError:
-            roles = ctx.guild.roles
-            for i in range(len(roles)):
-                if roles[i].name == args[1]:
-                    role_id = roles[i].id
-                    break
-            if role_id == None:
-                await ctx.reply("・Autoroleコマンド\nエラー：指定したロールが存在しません。")
+        if args[1] == "off":
+            if ctx.guild.id not in n_fc.autorole:
+                await ctx.reply("サーバーで自動ロールは設定されていません。")
                 return
-        if role_id == None:
-            await ctx.reply("・Autoroleコマンド\nエラー：指定したロールが存在しません。")
+            else:
+                del n_fc.autorole[ctx.guild.id]
+                save()
+                await ctx.reply("サーバーで自動ロールを無効にしました。")
+                return
+        elif args[1] == "on":
+            role_id = None
+            try:
+                role_id = int(args[2])
+            except ValueError:
+                roles = ctx.guild.roles
+                for i in range(len(roles)):
+                    if roles[i].name == args[2]:
+                        role_id = roles[i].id
+                        break
+                if role_id == None:
+                    await ctx.reply("自動ロール\nエラー：指定したロールが存在しません。")
+                    return
+            if role_id == None:
+                await ctx.reply("自動ロール\nエラー：指定したロールが存在しません。")
+                return
+            n_fc.autorole[ctx.guild.id] = role_id
+            await ctx.reply(embed=nextcord.Embed(title="自動ロール", description=f"設定完了：{ctx.guild.get_role(role_id).mention}を自動的に付与します。", color=0x00ff00))
+            save()
             return
-        n_fc.autorole[ctx.guild.id] = role_id
-        await ctx.reply(embed=nextcord.Embed(title="・Autoroleコマンド", description=f"設定完了：{ctx.guild.get_role(role_id).mention}を自動的に付与します。"))
-        save()
-        return
+        else:
+            embed = nextcord.Embed(title="自動ロール", description="コマンドの使用方法が不正です。\n`n!autorole on [ロール名又はロールID]`/`n!autorole off`", color=0x00ff00)
+            await ctx.reply(embed=embed)
+            return
     
     @commands.Cog.listener()
     async def on_member_join(self, member: nextcord.Member):
