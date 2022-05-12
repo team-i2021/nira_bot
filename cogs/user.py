10f3a6caeb7805dca2dabf1b10dc64e81f01433f
nattyan-tv <undirect_sbv@yahoo.co.jp>
2022-05-12 13:01:29 +0900

    `/ui` test

--- cogs/user.py
+++ cogs/user.py
@@ -68,11 +68,14 @@ class user(commands.Cog):
                 await ctx.message.reply(embed=nextcord.Embed(title="Error", description="ユーザーが存在しないか、データが取得できませんでした。", color=0xff0000))
                 return
     
+    @nextcord.slash_command(name="rk", description="ロールキーパー機能の設定")
+    async def rk_slash(self, interaction: Interaction):
+        pass
+
     @commands.command(name="rk", help="""\
-    ロールキーパー機能です。
-    大前提、**ちゃんと機能するとは思わないでください。**
-    `n!rk [on/off]`でロールキーパー機能の設定が可能です。ただ、ロールキーパー機能を有効にするには`n!ui`の、ユーザー情報表示を有効にしないと有効になりません。
-    """)
+ロールキーパー機能です。
+大前提、**ちゃんと機能するとは思わないでください。**
+`n!rk [on/off]`でロールキーパー機能の設定が可能です。ただ、ロールキーパー機能を有効にするには`n!ui`の、ユーザー情報表示を有効にしないと有効になりません。""")
     async def rk(self, ctx: commands.Context):
         if admin_check.admin_check(ctx.message.guild, ctx.message.author) or ctx.message.author.id in n_fc.py_admin:
             if ctx.message.content == "n!rk":
@@ -109,7 +112,7 @@ class user(commands.Cog):
             await ctx.reply("権限がありません。")
 
 
-    async def ui_config(bot: commands.Bot, interaction: Interaction or commands.Context, type: int, guild_id: int, channel):
+    async def ui_config(bot: commands.Bot, interaction: Interaction or commands.Context, type: int, guild_id: int, channel: nextcord.channel):
         if type == SET:
             try:
                 n_fc.welcome_id_list[guild_id] = channel.id
@@ -133,6 +136,47 @@ class user(commands.Cog):
             else:
                 return slash_tool.messages.mreply(interaction, embed=nextcord.Embed(title="ユーザー情報表示設定",description=f"サーバーで設定は`有効`です\n設定チャンネル: <#{n_fc.welcome_id_list[guild_id]}>", color=0x00ff00),ephemeral=True)
 
+    @nextcord.slash_command(name="ui", description="サーバー加入/離脱した場合にそのユーザーの情報を表示します")
+    async def ui_slash(self, interaction):
+        pass
+
+    @ui_slash.subcommand(name="set", description="ユーザー表示チャンネルをセットします")
+    async def set_slash(
+            self,
+            interaction: Interaction,
+            channel: nextcord.channel = SlashOption(
+                name="channel",
+                description="メッセージを送信するチャンネルです",
+                required=True
+            )
+        ):
+        if admin_check(interaction.guild, interaction.user):
+            await self.ui_config(self.bot, interaction, SET, interaction.guild.id, channel)
+        else:
+            await interaction.response.send_message("管理者権限がありません。", ephemeral=True)
+        return
+
+    @ui_slash.subcommand(name="del", description="ユーザー表示チャンネル設定を削除します")
+    async def del_slash(
+            self,
+            interaction: Interaction
+        ):
+        if admin_check(interaction.guild, interaction.user):
+            await self.ui_config(self.bot, interaction, DEL, interaction.guild.id, None)
+        else:
+            await interaction.response.send_message("管理者権限がありません。", ephemeral=True)
+        return
+
+    @ui_slash.subcommand(name="status", description="ユーザー表示チャンネル設定を確認します")
+    async def status_slash(
+            self,
+            interaction: Interaction
+        ):
+        if admin_check(interaction.guild, interaction.user):
+            await self.ui_config(self.bot, interaction, STATUS, interaction.guild.id, None)
+        else:
+            await interaction.response.send_message("管理者権限がありません。", ephemeral=True)
+        return
 
     @commands.command(name="ui", help="""\
     誰かがDiscordサーバーに加入/離脱した際に、指定したチャンネルにそのユーザーの情報を表示します。
@@ -145,7 +189,8 @@ class user(commands.Cog):
                 return
             elif mes_arg[1] == "set":
                 set_id = int("".join(re.findall(r'[0-9]', mes_arg[2])))
-                await self.ui_config(self.bot, ctx, SET, ctx.guild.id, set_id)
+                channel = await self.bot.fetch_channel(set_id)
+                await self.ui_config(self.bot, ctx, SET, ctx.guild.id, channel)
                 return
             elif  mes_arg[1] == "del":
                 await self.ui_config(self.bot, ctx, DEL, ctx.guild.id, None)
