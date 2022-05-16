c7401930d25c9582256ccc6cd6c73a68f900cbae
nattyan-tv <undirect_sbv@yahoo.co.jp>
2022-05-16 22:54:53 +0900

    TTSで辞書を追加したいはず

--- util/tts_convert.py
+++ util/tts_convert.py
@@ -1,8 +1,8 @@
 import re, sys, os
 from urlextract import URLExtract
+from util import tts_dict
 
 extractor = URLExtract()
-
 
 def convert(message: str) -> str:
     """TTS用に文章を構成し直す"""
@@ -11,6 +11,8 @@ def convert(message: str) -> str:
     urls = extractor.find_urls(message)
     for url in urls:
         message = message.replace(url, "、ゆーあーるえる、")
+    for word, read in tts_dict.main.items():
+        message = message.replace(word, read)
     message = message.replace("?","？").replace("&","あんど").replace("=","いこーる").replace("\n","。").replace("/", "すらっしゅ").replace(" ", "、").replace(" ", "、")
     message = re.sub("w{3,}", "わらわらわら", message)
     return message
