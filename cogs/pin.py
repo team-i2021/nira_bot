a3bc5107daeaed01b12573957ba84ba974874788
nattyan-tv <undirect_sbv@yahoo.co.jp>
2022-05-28 03:00:55 +0900

    add: slash command #15 （まだ普通コマンドも完成してないのに書いたスラコマ） `/pin`

--- cogs/pin.py
+++ cogs/pin.py
@@ -1,8 +1,6 @@
 import asyncio
 from nextcord.ext import commands, tasks
 import nextcord
-import subprocess
-from subprocess import PIPE
 import os
 import sys
 from nextcord import Interaction, SlashOption, ChannelType
@@ -65,37 +63,95 @@ offにするには、`n!pin off`と送信してください。
     async def pin(self, ctx: commands.Context):
         if ctx.author.id != 669178357371371522:
             return
-        args = ctx.message.content.split(" ", 2)
-        if len(args) == 1:
-            await ctx.reply("`n!pin on [メッセージ内容...(複数行可)]` と送ってください。")
-            return
-        if args[1] == "on":
-            if len(args) != 3:
-                await ctx.reply("・エラー\n引数が足りません。\n`n!pin on [メッセージ内容]`または`n!pin off`")
+        if admin_check.admin_check(ctx.guild, ctx.author):
+            args = ctx.message.content.split(" ", 2)
+            if len(args) == 1:
+                await ctx.reply("`n!pin on [メッセージ内容...(複数行可)]` と送ってください。")
                 return
-            if ctx.guild.id not in n_fc.pinMessage:
-                n_fc.pinMessage[ctx.guild.id] = {ctx.channel.id: args[2]}
+            if args[1] == "on":
+                if len(args) != 3:
+                    await ctx.reply("・エラー\n引数が足りません。\n`n!pin on [メッセージ内容]`または`n!pin off`")
+                    return
+                if ctx.guild.id not in n_fc.pinMessage:
+                    n_fc.pinMessage[ctx.guild.id] = {ctx.channel.id: args[2]}
+                else:
+                    n_fc.pinMessage[ctx.guild.id][ctx.channel.id] = args[2]
+                await ctx.message.add_reaction("\U0001F197")
+                await ctx.reply("Ok")
+            elif args[1] == "off":
+                if ctx.guild.id not in n_fc.pinMessage or ctx.channel.id not in n_fc.pinMessage[ctx.guild.id]:
+                    await ctx.reply("このチャンネルにはpinメッセージはありません。")
+                    return
+                else:
+                    messages = await ctx.channel.history(limit=10).flatten()
+                    search_message = n_fc.pinMessage[ctx.guild.id][ctx.channel.id]
+                    del n_fc.pinMessage[ctx.guild.id][ctx.channel.id]
+                    for i in messages:
+                        if i.content == search_message:
+                            await i.delete()
+                    await ctx.reply("登録を解除しました。")
+                    return
             else:
-                n_fc.pinMessage[ctx.guild.id][ctx.channel.id] = args[2]
-            await ctx.message.add_reaction("\U0001F197")
-            await ctx.reply("Ok")
-        elif args[1] == "off":
-            if ctx.guild.id not in n_fc.pinMessage or ctx.channel.id not in n_fc.pinMessage[ctx.guild.id]:
-                await ctx.reply("このチャンネルにはpinメッセージはありません。")
+                await ctx.reply("・エラー\n使い方が違います。\n`n!pin on [メッセージ内容]`または`n!pin off`")
                 return
-            else:
-                messages = await ctx.channel.history(limit=10).flatten()
-                search_message = n_fc.pinMessage[ctx.guild.id][ctx.channel.id]
-                del n_fc.pinMessage[ctx.guild.id][ctx.channel.id]
-                for i in messages:
-                    if i.content == search_message:
-                        await i.delete()
-                await ctx.reply("登録を解除しました。")
-                return
-        else:
-            await ctx.reply("・エラー\n使い方が違います。\n`n!pin on [メッセージ内容]`または`n!pin off`")
+        else: await ctx.reply("あなたには権限がありません。"); return
+
+
+    @nextcord.slash_command(name="pin", description="メッセージ下部ピン留め機能", guild_ids=n_fc.GUILD_IDS)
+    async def pin_slash(self, interaction: Interaction):
+        pass
+
+
+    @pin_slash.subcommand(name="on", description="メッセージ下部ピン留め機能をONにする")
+    async def on_slash(
+            self,
+            interaction: Interaction,
+            messageContent: str = SlashOption(
+                name="messageContent",
+                description="ピン留めするメッセージ",
+                required=True,
+            ),
+        ):
+        if interaction.user.id != 669178357371371522:
+            await interaction.response.send_message("This command could used by admin.", ephemeral=True)
             return
-    
+        await interaction.response.defer()
+        if admin_check.admin_check(interaction.guild, interaction.author):
+            if interaction.guild.id not in n_fc.pinMessage:
+                n_fc.pinMessage[interaction.guild.id] = {interaction.channel.id: messageContent}
+            else:
+                n_fc.pinMessage[interaction.guild.id][interaction.channel.id] = messageContent
+            await interaction.followup.send("設定を変更しました。", ephemeral=True)
+        else: await interaction.followup.send("あなたには管理者権限がありません。", ephemeral=True); return
+
+
+    @pin_slash.subcommand(name="off", description="メッセージ下部ピン留め機能をOFFにする")
+    async def off_slash(
+            self,
+            interaction: Interaction,
+        ):
+        if interaction.user.id != 669178357371371522:
+            await interaction.response.send_message("This command could used by admin.", ephemeral=True)
+            return
+        await interaction.response.defer()
+        if admin_check.admin_check(interaction.guild, interaction.author):
+            try:
+                if interaction.guild.id not in n_fc.pinMessage or interaction.channel.id not in n_fc.pinMessage[interaction.guild.id]:
+                    await interaction.followup.send("このチャンネルにはpinメッセージはありません。", ephemeral=True)
+                else:
+                    messages = await interaction.channel.history(limit=10).flatten()
+                    search_message = n_fc.pinMessage[interaction.guild.id][interaction.channel.id]
+                    del n_fc.pinMessage[interaction.guild.id][interaction.channel.id]
+                    for i in messages:
+                        if i.content == search_message:
+                            await i.delete()
+                    await interaction.followup.send("登録を解除しました。", ephemeral=True)
+            except BaseException as err:
+                await interaction.followup.send(f"エラーが発生しました。\n```sh\n{err}```", ephemeral=True)
+                return
+        else: await interaction.followup.send("あなたには管理者権限がありません。", ephemeral=True); return
+
+
     @tasks.loop(seconds=3)
     async def checkPin(self):
         await self.bot.wait_until_ready()
