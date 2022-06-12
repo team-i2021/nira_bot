c8c89171cb82141678e000bce550b1d89d8d0ebf
nattyan-tv <undirect_sbv@yahoo.co.jp>
2022-06-13 00:53:08 +0900

    キモいと言われたので削除 （remove `(´・ω・｀)`'s reaction）

--- cogs/normal_reaction.py
+++ cogs/normal_reaction.py
@@ -1,3 +1,5 @@
+import util.srtr as srtr
+from util import admin_check, n_fc, eh, web_api
 from nextcord.ext import commands
 import nextcord
 import datetime
@@ -12,28 +14,30 @@ import os
 
 import logging
 dir = sys.path[0]
+
+
 class NoTokenLogFilter(logging.Filter):
     def filter(self, record):
         message = record.getMessage()
         return 'token' not in message
 
+
 logger = logging.getLogger(__name__)
 logger.addFilter(NoTokenLogFilter())
 formatter = '%(asctime)s$%(filename)s$%(lineno)d$%(funcName)s$%(levelname)s:%(message)s'
-logging.basicConfig(format=formatter, filename=f'{dir}/nira.log', level=logging.INFO)
+logging.basicConfig(
+    format=formatter, filename=f'{dir}/nira.log', level=logging.INFO)
 
 
 home_dir = os.path.dirname(__file__)[:-4]
 
 
 sys.path.append('../')
-from util import admin_check, n_fc, eh, web_api
-import util.srtr as srtr
 
 image_root = "https://team-i2021.github.io/nira_bot/images"
 image_loc = f"{dir}/images"
 
-#通常反応をまとめたもの。
+# 通常反応をまとめたもの。
 
 nira_hantei = r'にら|ニラ|nira|garlic|韮|Chinese chives|Allium tuberosum|てりじの|テリジノ'
 
@@ -94,66 +98,68 @@ reaction_list = [
 # ファイル/反応数/0
 # 反応ファイルなど
 reaction_files = {
-    0:[0,1,"おっ、かわいいな"],
-    1:[0,1,"かわいいですね..."],
-    2:[0,1,"くそわろたｧ!!!"],
-    3:[0,1,"にーっこにこ動画？"],
-    4:[0,1,"ようつべぇ...？"],
-    5:[0,1,"ついったぁ！！！"],
-    6:[1,1,"nira_zoku"],
-    7:[1,6,"itokoi"],
-    8:[2,2,"nira_saibai"],
-    9:[2,1,"nira_itou"],
-    10:[2,1,"nira_rice"],
-    11:[2,1,"nira_kare"],
-    12:[2,1,"nira_fish"],
-    13:[2,1,"nira_dokusai"],
-    14:[2,1,"nira_grow"],
-    15:[3,1,"https://cookpad.com/search/%E3%83%8B%E3%83%A9%E9%8D%8B"],
-    16:[2,1,"nira_sword"],
-    17:[2,1,"nira_and"],
-    18:[3,1,"https://sites.google.com/view/nirand"],
-    19:[2,1,"nira_manju"],
-    20:[2,1,"rebanira"],
-    21:[2,2,"nira_tori"],
-    22:[3,1,"https://twitter.com/niranuranura"],
-    23:[1,4,"nira"],
-    24:[4,2,"tiramisu"],
-    25:[1,3,"pino"],
-    26:[4,2,"fox"],
-    27:[1,1,"uiro"],
-    28:[0,1,"https://www.youtube.com/IkumonTV"],
-    29:[4,1,"apple"],
-    30:[4,3,"sarmon"],
-    31:[6,0,"<:natsu:908565532268179477>"],
-    32:[4,1,"12pp"],
-    33:[5,2,"https://twitter.com/namae_1216",":heart: by apple"],
-    34:[1,1,"mike"],
-    35:[4,1,"au"],
-    36:[1,1,"serori"],
-    37:[4,2,"rori"],
-    38:[5,2,"全知全能なすごい人。\n多分お金と時間があれば何でもできる","毎晩10時は..."],
-    39:[0,1,"ﾜｰｽｹﾞｪｽｯｹﾞｸｧｯｸｧｯｸｧwwwww"],
-    40:[0,1,"https://www.google.com/search?q=%E3%81%93%E3%81%AE%E8%BF%91%E3%81%8F%E3%81%AE%E7%84%BC%E3%81%8D%E9%B3%A5%E5%B1%8B"],
-    41:[1,1,"kawaisou"],
-    42:[5,1,"https://twitter.com/ribpggxcrmz74t6"],
-    43:[5,1,"https://twitter.com/Aibell__game"],
-    44:[1,1,"sabakan"],
-    45:[4,1,"sosyou"],
-    46:[1,1,"syuuzin"],
-    47:[1,2,"bsod"],
-    48:[0,1,"""滲み出す混濁の紋章、不遜なる狂気の器、
+    0: [0, 1, "おっ、かわいいな"],
+    1: [0, 1, None],
+    2: [0, 1, "くそわろたｧ!!!"],
+    3: [0, 1, "にーっこにこ動画？"],
+    4: [0, 1, "ようつべぇ...？"],
+    5: [0, 1, "ついったぁ！！！"],
+    6: [1, 1, "nira_zoku"],
+    7: [1, 6, "itokoi"],
+    8: [2, 2, "nira_saibai"],
+    9: [2, 1, "nira_itou"],
+    10: [2, 1, "nira_rice"],
+    11: [2, 1, "nira_kare"],
+    12: [2, 1, "nira_fish"],
+    13: [2, 1, "nira_dokusai"],
+    14: [2, 1, "nira_grow"],
+    15: [3, 1, "https://cookpad.com/search/%E3%83%8B%E3%83%A9%E9%8D%8B"],
+    16: [2, 1, "nira_sword"],
+    17: [2, 1, "nira_and"],
+    18: [3, 1, "https://sites.google.com/view/nirand"],
+    19: [2, 1, "nira_manju"],
+    20: [2, 1, "rebanira"],
+    21: [2, 2, "nira_tori"],
+    22: [3, 1, "https://twitter.com/niranuranura"],
+    23: [1, 4, "nira"],
+    24: [4, 2, "tiramisu"],
+    25: [1, 3, "pino"],
+    26: [4, 2, "fox"],
+    27: [1, 1, "uiro"],
+    28: [0, 1, "https://www.youtube.com/IkumonTV"],
+    29: [4, 1, "apple"],
+    30: [4, 3, "sarmon"],
+    31: [6, 0, "<:natsu:908565532268179477>"],
+    32: [4, 1, "12pp"],
+    33: [5, 2, "https://twitter.com/namae_1216", ":heart: by apple"],
+    34: [1, 1, "mike"],
+    35: [4, 1, "au"],
+    36: [1, 1, "serori"],
+    37: [4, 2, "rori"],
+    38: [5, 2, "全知全能なすごい人。\n多分お金と時間があれば何でもできる", "毎晩10時は..."],
+    39: [0, 1, "ﾜｰｽｹﾞｪｽｯｹﾞｸｧｯｸｧｯｸｧwwwww"],
+    40: [0, 1, "https://www.google.com/search?q=%E3%81%93%E3%81%AE%E8%BF%91%E3%81%8F%E3%81%AE%E7%84%BC%E3%81%8D%E9%B3%A5%E5%B1%8B"],
+    41: [1, 1, "kawaisou"],
+    42: [5, 1, "https://twitter.com/ribpggxcrmz74t6"],
+    43: [5, 1, "https://twitter.com/Aibell__game"],
+    44: [1, 1, "sabakan"],
+    45: [4, 1, "sosyou"],
+    46: [1, 1, "syuuzin"],
+    47: [1, 2, "bsod"],
+    48: [0, 1, """滲み出す混濁の紋章、不遜なる狂気の器、
 湧き上がり・否定し・痺れ・瞬き・眠りを妨げる
 爬行する鉄の王女　絶えず自壊する泥の人形
 結合せよ　反発せよ　地に満ち己の無力を知れ　破道の九十・黒棺！！！！！！
 """],
-    49:[0,1,"https://cookpad.com/search/%E4%BB%8A%E6%97%A5%E3%81%AE%E3%83%A9%E3%83%B3%E3%83%81"],
-    }
+    49: [0, 1, "https://cookpad.com/search/%E4%BB%8A%E6%97%A5%E3%81%AE%E3%83%A9%E3%83%B3%E3%83%81"],
+}
 
 # すべてが許されるGuild
-allow_guild = [870642671415337001,906400213495865344]
+allow_guild = [870642671415337001, 906400213495865344]
 
 # 通常反応をプログラム化したもの
+
+
 def n_reaction(message: nextcord.Message, *custom: int):
     if custom == ():
         ans = listre.search(reaction_list, message.content)
@@ -166,7 +172,10 @@ def n_reaction(message: nextcord.Message, *custom: int):
         # 通常テキスト
         if reaction_files[ans[1]][0] == 0:
             if reaction_files[ans[1]][1] == 1:
-                return message.reply(reaction_files[ans[1]][2])
+                if reaction_files[ans[1]][2] is not None:
+                    return message.reply(reaction_files[ans[1]][2])
+                else:
+                    return asyncio.sleep(0)
             else:
                 rnd = random.randint(1, reaction_files[ans[1]][1])
                 return message.reply(reaction_files[ans[1]][rnd+1])
@@ -261,7 +270,8 @@ class normal_reaction(commands.Cog):
             return
         if message.guild.id in n_fc.notify_token:
             if message.channel.id in n_fc.notify_token[message.guild.id]:
-                web_api.notify_line(message, n_fc.notify_token[message.guild.id][message.channel.id])
+                web_api.notify_line(
+                    message, n_fc.notify_token[message.guild.id][message.channel.id])
         # 自分自身(BOT)には反応しない
         if message.author.bot:
             return
@@ -294,7 +304,8 @@ class normal_reaction(commands.Cog):
         # 通常反応のブール値存在チェック #
         ###############################
         if message.guild.id not in n_fc.reaction_bool_list:
-            n_fc.reaction_bool_list[message.guild.id] = {"all": 1, message.channel.id: 1}
+            n_fc.reaction_bool_list[message.guild.id] = {
+                "all": 1, message.channel.id: 1}
             with open(f'{home_dir}/reaction_bool_list.nira', 'wb') as f:
                 pickle.dump(n_fc.reaction_bool_list, f)
         if message.channel.id not in n_fc.reaction_bool_list[message.guild.id]:
@@ -322,6 +333,7 @@ class normal_reaction(commands.Cog):
                     return
                 except BaseException:
                     return
+
 
 def setup(bot):
     bot.add_cog(normal_reaction(bot))
