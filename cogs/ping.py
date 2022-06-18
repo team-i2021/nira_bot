3a1506f03678cf11ade17b5133f21fc719a74fd4
nattyan-tv <undirect_sbv@yahoo.co.jp>
2022-06-19 02:34:44 +0900

    change: pingの仕様変更

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,3 +1,5 @@
+import logging
+from util import admin_check, n_fc, eh
 import asyncio
 from nextcord.ext import commands
 import nextcord
@@ -7,100 +9,107 @@ from subprocess import PIPE
 import os
 import sys
 from nextcord import Interaction, SlashOption, ChannelType
+import json
+import HTTP_db
+import platform
 
 sys.path.append('../')
-from util import admin_check, n_fc, eh
-#pingを送信するだけ
+# pingを送信するだけ
 
+DISCORD = 0xD13C06D
 
-
-#loggingの設定
-import logging
+# loggingの設定
 dir = sys.path[0]
+
+
 class NoTokenLogFilter(logging.Filter):
     def filter(self, record):
         message = record.getMessage()
         return 'token' not in message
 
+
 logger = logging.getLogger(__name__)
 logger.addFilter(NoTokenLogFilter())
 formatter = '%(asctime)s$%(filename)s$%(lineno)d$%(funcName)s$%(levelname)s:%(message)s'
-logging.basicConfig(format=formatter, filename=f'{dir}/nira.log', level=logging.INFO)
+logging.basicConfig(
+    format=formatter, filename=f'{dir}/nira.log', level=logging.INFO)
 
-async def ping_there(ctx, adr, type):
-    if type == 1:
-        async with ctx.channel.typing():
-            res = subprocess.run(f"ping {adr} -c 4 -W 3", stdout=PIPE, stderr=PIPE, shell=True, text=True)
-            if res.returncode == 0:
-                await ctx.reply(f"`{adr}`に接続できました。\n```{res.stdout}```")
-            else:
-                await ctx.reply(f"`{adr}`に接続できませんでした。")
-            return
-    elif type == 0:
-        res = subprocess.run(f"ping {adr} -c 4 -W 3", stdout=PIPE, stderr=PIPE, shell=True, text=True)
-        if res.returncode == 0:
-            await ctx.followup.send(f"`{adr}`に接続できました。\n```{res.stdout}```")
-        else:
-            await ctx.followup.send(f"`{adr}`に接続できませんでした。")
 
-#ctx.message == None:こまんど
-async def base_ping(bot, ctx: commands.Context, adr):
-    if type(ctx) == Interaction and adr == None:
-        logger.info(f"DiscordサーバーとのPing値：{round(bot.latency * 1000)}ms")
-        await ctx.followup.send(embed=nextcord.Embed(title="Ping", description=f"現在のPing値は`{round(bot.latency * 1000)}`msです。", color=0x00ff00))
-        return
-    elif type(ctx) == Interaction and adr != None:
-        if adr[8:] == "192.168." or adr == "127.0.0.1" or adr == "localhost" or adr == "0.0.0.0" or adr == "127.0.1" or adr == "127.1" or adr == "2130706433" or adr == "0x7F000001" or adr == "017700000001":
-            if ctx.author.id not in n_fc.py_admin:
-                await ctx.followup.send(embed=nextcord.Embed(title="Ping", description=f"localhostおよびLAN内にはPingできません\n||多分...||",color=0xff0000),ephemeral=True)
-                return
+async def ping_there(adr, message: nextcord.Message):
+    command = ""
+    if platform.system() == "Windows":
+        command = f"ping {adr} -n 3 -w 3"
+    elif platform.system() == "Linux":
+        command = f"ping {adr} -c 3 -W 3"
+    else:
+        print(
+            f"Unsupported OS: {platform.system()}\nPing command may not work.")
+        command = f"ping {adr} -c 3 -W 3"
+    res = subprocess.run(
+        command,
+        stdout=PIPE,
+        stderr=PIPE,
+        shell=True,
+        text=True
+    )
+    if res.returncode == 0:
+        await message.edit(embed=nextcord.Embed(title="Ping", description=f"`{adr}`に時間内に接続できました。\n```sh\n{res.stdout}```", color=0x00ff00))
+    else:
+        await message.edit(embed=nextcord.Embed(title="Ping", description=f"`{adr}`に時間内に接続できませんでした。", color=0x00ff00))
+
+# ctx.message == None:こまんど
+
+
+async def base_ping(bot, client: HTTP_db.Client, adr, message: nextcord.Message):
+    if adr == DISCORD:
+        bot_latency = round(bot.latency * 1000, 2)
+        await message.edit(embed=nextcord.Embed(title="Ping", description=f"- Discord Server\n`{bot_latency}`ms\n\n- Database Server\n`Connecting...`", color=0x00ff00))
         try:
-            await asyncio.wait_for(ping_there(ctx, adr, 0), timeout=8)
+            data = await client.ping()
+            db_latency = round(float(data.ping), 2)
+            await message.edit(embed=nextcord.Embed(title="Ping", description=f"- Discord Server\n`{bot_latency}`ms\n\n- Database Server\n`{db_latency}`ms", color=0x00ff00))
+            return
+        except HTTP_db.UnknownDatabaseError:
+            await message.edit(embed=nextcord.Embed(title="Ping", description=f"- Discord Server\n`{bot_latency}`ms\n\n- Database Server\n`Connection Error.`", color=0x00ff00))
+            return
+
+    elif adr != DISCORD:
+        if adr[8:] == "192.168." or adr == "127.0.0.1" or adr == "localhost" or adr == "0.0.0.0" or adr == "127.0.1" or adr == "127.1" or adr == "2130706433" or adr == "0x7F000001" or adr == "017700000001":
+            await message.edit(embed=nextcord.Embed(title="Ping", description=f"localhostおよびLAN内にはPingできません\n||多分...||", color=0x00ff00))
+            return
+        try:
+            await asyncio.wait_for(ping_there(adr, message), timeout=8)
             return
         except BaseException as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            logging.error(f"大変申し訳ございません。エラーが発生しました。\n```{err}```\n```sh\n{sys.exc_info()}```\nfile:`{fname}`\nline:{exc_tb.tb_lineno}")
-            await ctx.followup.send(embed=nextcord.Embed(title="Ping", description=f"エラーが発生しました。\n```sh\n{traceback.format_exc()}```",color=0xff0000),ephemeral=True)
-            # await ctx.respond(f"{adr}に時間内に接続できませんでした。")
+            logging.error(
+                f"大変申し訳ございません。エラーが発生しました。\n```{err}```\n```sh\n{sys.exc_info()}```\nfile:`{fname}`\nline:{exc_tb.tb_lineno}")
+            await message.edit(embed=nextcord.Embed(title="Ping", description=f"内部エラーが発生しました。\n```py\n{traceback.format_exc()}```", color=0xff0000))
         return
-    if ctx.message != None:
-        if adr == None:
-            embed = nextcord.Embed(title="Ping", description=f"現在のPing値は`{round(bot.latency * 1000)}`msです。", color=0x00ff00)
-            logger.info(f"DiscordサーバーとのPing値：{round(bot.latency * 1000)}ms")
-            await ctx.reply(embed=embed)
-            return
-        elif ctx.message.content != "n!ping":
-            if adr[8:] == "192.168." or adr == "127.0.0.1" or adr == "localhost" or adr == "0.0.0.0" or adr == "127.0.1" or adr == "127.1" or adr == "2130706433" or adr == "0x7F000001" or adr == "017700000001":
-                if ctx.message.author.id not in n_fc.py_admin:
-                    await ctx.reply("localhost及びローカルIPにはping出来ません。")
-                    return
-            try:
-                await asyncio.wait_for(ping_there(ctx, adr, 1), timeout=8)
-                return
-            except asyncio.TimeoutError:
-                await ctx.reply(f"{adr}に時間内に接続できませんでした。")
-            return
-        return
-    return
 
 
 class ping(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        datas = json.load(open(f'{dir}/setting.json', 'r'))["database_data"]
+        self.client = HTTP_db.Client(
+            url=datas["address"], port=datas["port"], intkey=True)
 
     @nextcord.slash_command(name="ping", description="Discordサーバー又は、指定サーバーとのレイテンシを計測します。", guild_ids=n_fc.GUILD_IDS)
     async def ping_slash(
-            self,
-            interaction = Interaction,
-            address: str = SlashOption(
-                name="address",
-                description="アドレス。指定しないとDiscordサーバーとのRTTを表示します。",
-                required=False,
-            )
-        ):
-        await interaction.response.defer()
-        await base_ping(self.bot, interaction, address)
+        self,
+        interaction=Interaction,
+        address: str = SlashOption(
+            name="address",
+            description="アドレス。指定しないとDiscordサーバーとのRTTを表示します。",
+            required=False,
+        )
+    ):
+        if address is None:
+            address = DISCORD
+        message = await interaction.response.send_message(embed=nextcord.Embed(title="Ping", description=f"Ping測定中...", color=0x00ff00))
+        await base_ping(self.bot, self.client, address, message)
 
     @commands.command(name="ping", help="""\
 レイテンシを表示します。
@@ -112,11 +121,13 @@ class ping(commands.Cog):
 引数1:address(str)
 IPアドレスまたはドメインの形で指定してください。""")
     async def ping(self, ctx: commands.Context):
-        texts = ctx.message.content.split(" ",1)
+        message = await ctx.reply(embed=nextcord.Embed(title="Ping", description=f"Ping測定中...", color=0x00ff00))
+        texts = ctx.message.content.split(" ", 1)
         if len(texts) == 1:
-            await base_ping(self.bot, ctx, None)
+            await base_ping(self.bot, self.client, DISCORD, message)
         else:
-            await base_ping(self.bot, ctx, texts[1])
+            await base_ping(self.bot, self.client, texts[1], message)
+
 
 def setup(bot):
     bot.add_cog(ping(bot))
