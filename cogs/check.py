785bbcac6fd70bdf5bece1be1a618894669b30e1
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2022-06-20 09:41:36 +0900

    fix: reference destination of the id was different

--- cogs/check.py
+++ cogs/check.py
@@ -16,7 +16,7 @@ class check(commands.Cog):
     async def admin_user(self, interaction: Interaction, member: nextcord.Member):
         if admin_check.admin_check(member.guild, member):
             await slash_tool.messages.mreply(interaction, "", embed=nextcord.Embed(title="ADMIN", description=f"権限があるようです。", color=0x00ff00))
-        elif interaction.user.id in n_fc.py_admin:
+        elif member.id in n_fc.py_admin:
             await slash_tool.messages.mreply(interaction, "", embed=nextcord.Embed(title="ADMIN", description=f"サーバー権限はありませんが、コマンドは実行できます。（BOT開発者）", color=0xffff00))
         else:
             await slash_tool.messages.mreply(interaction, "", embed=nextcord.Embed(title="ADMIN", description=f"権限がないようです。\n**（管理者権限を付与したロールがありませんでした。）**\n自分が管理者の場合は、自分に管理者権限を付与したロールを付けてください。", color=0xff0000))
