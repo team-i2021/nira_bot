73763552ea11a1ce36a062a44dd1ce0555722578
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2022-12-10 18:13:26 +0900

    add: user.pyをmongo化

--- cogs/user.py
+++ cogs/user.py
@@ -6,7 +6,9 @@ import sys
 import HTTP_db
 import nextcord
 from nextcord import Interaction, SlashOption
-from nextcord.ext import commands
+from nextcord.ext import commands, application_checks
+
+from motor import motor_asyncio
 
 from util import admin_check, eh, slash_tool, database, n_fc
 from util.nira import NIRA
@@ -16,23 +18,11 @@ SET, DEL, STATUS = (0, 1, 2)
 # ユーザー情報表示系
 
 
-class RoleKeeper:
-    name = "role_keeper"
-    value = {}
-    default = {}
-    value_type = database.CHANNEL_VALUE
-
-
-class WelcomeInfo:
-    name = "welcome_info"
-    value = {}
-    default = {}
-    value_type = database.GUILD_VALUE
-
-
-class user(commands.Cog):
+class User(commands.Cog):
     def __init__(self, bot: NIRA, **kwargs):
         self.bot = bot
+        self.rk_collection: motor_asyncio.AsyncIOMotorCollection = self.bot.database["role_keeper"]
+        self.winfo_collection: motor_asyncio.AsyncIOMotorCollection = self.bot.database["welcome_info"]
 
     def UserInfoEmbed(self, member: nextcord.Member or nextcord.User):
         if member.bot:
@@ -110,21 +100,18 @@ class user(commands.Cog):
 `n!d [ユーザーID]`という風に指定すると、そのユーザーの情報を表示することが出来ます。
 ユーザーIDの指定方法は...多分調べれば出てきます。
 **メンションでも指定できますが、いざこざとかにつながるかもしれないのでしないほうが得策です。**""")
-    async def d(self, ctx: commands.Context):
-        arg = ctx.message.content.split(" ", 1)
-        if len(arg) == 1:
+    async def show_user(self, ctx: commands.Context, req_user: int | None = None):
+        if req_user is None:
             user = await self.bot.fetch_user(ctx.author.id)
             await ctx.reply(embed=self.UserInfoEmbed(user))
         else:
-            check_id = int(
-                "".join(re.findall(r'\d', arg[1])))
             try:
-                user = await self.bot.fetch_user(check_id)
+                user = await self.bot.fetch_user(req_user)
                 await ctx.reply(embed=self.UserInfoEmbed(user))
                 return
             except Exception:
                 try:
-                    channel = await self.bot.fetch_channel(check_id)
+                    channel = await self.bot.fetch_channel(req_user)
                     if type(channel) == nextcord.channel.TextChannel:
                         embed = nextcord.Embed(
                             title="TextChannel Info",
@@ -170,7 +157,7 @@ class user(commands.Cog):
                 except Exception:
                     try:
                         # Forbidden.....?????
-                        guild = await self.bot.fetch_guild(check_id)
+                        guild = await self.bot.fetch_guild(req_user)
                         embed = nextcord.Embed(
                             title="Guild Info",
                             description=f"名前:`{guild.name}`\nID:`{guild.id}`\n人数:`{len(guild.members)}`人(そのうちのBOT数:`{len(guild.bots)}`)\nオーナー:{guild.owner.mention}",
@@ -190,105 +177,67 @@ class user(commands.Cog):
                         await ctx.reply(embed=nextcord.Embed(title="Error", description="ユーザー、チャンネル、またはサーバーのデータが取得できませんでした。", color=0xff0000))
                         return
 
+    @application_checks.has_permissions(manage_guild=True)
     @nextcord.slash_command(name="rk", description="Setting of role-keeper", description_localizations={nextcord.Locale.ja: "ロールキーパーの設定"}, guild_ids=n_fc.GUILD_IDS)
     async def rk_slash(
         self,
         interaction: Interaction,
-        SettingValue: int = SlashOption(
+        SettingValue: bool = SlashOption(
             name="setting_value",
             name_localizations={nextcord.Locale.ja: "設定値"},
             description="Value of setting",
             description_localizations={nextcord.Locale.ja: "設定値"},
             required=True,
-            choices={"有効にする": 1, "無効にする": 0}
+            choices={"有効にする": True, "無効にする": False}
         )
     ):
-        if admin_check.admin_check(interaction.guild, interaction.user):
-            await database.default_pull(self.bot.client, RoleKeeper)
-            if interaction.guild.id in RoleKeeper.value:
-                RoleKeeper.value[interaction.guild.id]["rk"] = SettingValue
-                await interaction.response.send_message(embed=nextcord.Embed(title="Role Keeper", description="設定を変更しました。", color=0x00ff00))
-            else:
-                RoleKeeper.value[interaction.guild.id] = {"rk": SettingValue}
-                await interaction.response.send_message(embed=nextcord.Embed(title="Role Keeper", description="設定を変更しました。", color=0x00ff00))
-            await database.default_push(self.bot.client, RoleKeeper)
-        else:
-            await interaction.response.send_message(embed=nextcord.Embed(title="Error", description="権限がありません。", color=0xff0000))
+        await interaction.response.defer(ephemeral=True)
+        await self.rk_collection.update_one({"guild_id": interaction.guild.id}, {"$set": {"setting": SettingValue}})
+        await interaction.send(f"ロールキーパー機能を{'有効' if SettingValue else '無効'}にしました。\n※この機能への過信はやめましょう。", ephemeral=True)
 
-
+    @commands.has_permissions(manage_guild=True)
     @commands.command(name="rk", help="""\
 ロールキーパー機能です。
-大前提、**ちゃんと機能するとは思わないでください。**
-`n!rk [on/off]`でロールキーパー機能の設定が可能です。ただ、ロールキーパー機能を有効にするには`n!ui`の、ユーザー情報表示を有効にしないと有効になりません。""")
-    async def rk(self, ctx: commands.Context):
-        if admin_check.admin_check(ctx.guild, ctx.author):
-            if ctx.message.content == f"{self.bot.command_prefix}rk":
-                embed = nextcord.Embed(
-                    title="ロールキーパー",
-                    description=f"`{self.bot.command_prefix}rk [on/off]`",
-                    color=0x00ff00
-                )
-                await database.default_pull(self.bot.client, RoleKeeper)
-                if ctx.guild.id not in RoleKeeper.value:
-                    RoleKeeper.value[ctx.guild.id] = {"rk": 0}
-                    await database.default_push(self.bot.client, RoleKeeper)
-                if RoleKeeper.value[ctx.guild.id]["rk"] == 0:
-                    role_bool = "無効"
-                else:
-                    role_bool = "有効"
-                embed.add_field(
-                    name="ロールキーパーの状態",
-                    value=f"ロールキーパーは現在{role_bool}です"
-                )
-                await ctx.reply(embed=embed)
-                return
-            if ctx.message.content.split(" ", 1)[1] in n_fc.on_ali:
-                # ロールキーパーオンにしやがれ
-                await database.default_pull(self.bot.client, RoleKeeper)
-                if ctx.guild.id not in RoleKeeper.value:
-                    RoleKeeper.value[ctx.guild.id] = {"rk": 1}
-                else:
-                    RoleKeeper.value[ctx.guild.id]["rk"] = 1
-            elif ctx.message.content.split(" ", 1)[1] in n_fc.off_ali:
-                # ロールキーパーオフにしやがれ
-                if ctx.guild.id not in RoleKeeper.value:
-                    RoleKeeper.value[ctx.guild.id] = {"rk": 0}
-                else:
-                    RoleKeeper.value[ctx.guild.id]["rk"] = 0
-            else:
-                await ctx.reply(f"値が不正です。\n`{self.bot.command_prefix}rk [on/off]`")
-                return
-            await database.default_push(self.bot.client, RoleKeeper)
-            await ctx.reply("ロールキーパーの設定を更新しました。")
-        else:
-            await ctx.reply("権限がありません。")
+AutoMod等の機能を活用したうえで、過信しすぎずに使用してください。
 
-    async def ui_config(self, bot: commands.Bot, client: HTTP_db.Client, interaction: Interaction or commands.Context, type: int, guild_id: int, channel: nextcord.channel):
-        await database.default_pull(client, WelcomeInfo)
+`n!rk [on/off]`でロールキーパー機能の設定が可能です。ただ、ロールキーパー機能を有効にするには`n!ui`の、ユーザー情報表示を有効にしないと有効になりません。""")
+    async def rk(self, ctx: commands.Context, setting: str | None = None):
+        if setting is None:
+            result = await self.rk_collection.find_one({"guild_id": ctx.guild.id})
+            await ctx.reply(f"ロールキーパー機能は{'有効' if result['setting'] else '無効'}です。")
+        elif setting in n_fc.on_ali:
+            await self.rk_collection.update_one({"guild_id": ctx.guild.id}, {"$set": {"setting": True}})
+            await ctx.reply("ロールキーパー機能を有効にしました。\n**※本機能への過信はおやめください。**")
+        elif setting in n_fc.off_ali:
+            await self.rk_collection.update_one({"guild_id": ctx.guild.id}, {"$set": {"setting": False}})
+            await ctx.reply("ロールキーパー機能を無効にしました。")
+        else:
+            await ctx.reply(f"引数が不正です。\n`{ctx.prefix}rk [on/off]`")
+
+    async def ui_config(self, interaction: Interaction or commands.Context, type: int, guild_id: int, channel: nextcord.channel):
+        result = await self.ui_collection.find_one({"guild_id": guild_id})
         if type == SET:
             try:
-                WelcomeInfo.value[guild_id] = channel.id
-                CHANNEL = await bot.fetch_channel(WelcomeInfo.value[guild_id])
+                await self.ui_collection.update_one({"guild_id": guild_id}, {"$set": {"channel_id": channel.id}}, upsert=True)
+                CHANNEL = await self.bot.fetch_channel(channel.id)
                 await CHANNEL.send("このチャンネルが、ユーザー情報表示チャンネルとして指定されました。")
                 await slash_tool.messages.mreply(interaction, "", embed=nextcord.Embed(title="ユーザー情報表示設定", description=f"<#{channel.id}>に指定されました。", color=0x00ff00), ephemeral=True)
             except Exception as err:
                 await slash_tool.messages.mreply(interaction, "", embed=nextcord.Embed(title="ユーザー情報表示設定", description=f"エラーが発生しました。\n```\n{err}```", color=0xff000), ephemeral=True)
         elif type == DEL:
             try:
-                if guild_id not in WelcomeInfo.value:
+                if result is None:
                     await slash_tool.messages.mreply(interaction, "", embed=nextcord.Embed(title="ユーザー情報表示設定", description=f"このサーバーには登録されていません。", color=0xff0000), ephemeral=True)
                     return
-                del WelcomeInfo.value[guild_id]
+                await self.ui_collection.delete_one({"guild_id": guild_id})
                 await slash_tool.messages.mreply(interaction, "", embed=nextcord.Embed(title="ユーザー情報表示設定", description=f"設定を削除しました。", color=0x00ff00), ephemeral=True)
             except Exception as err:
                 await slash_tool.messages.mreply(interaction, "", embed=nextcord.Embed(title="ユーザー情報表示設定", description=f"エラーが発生しました。\n```\n{err}```", color=0xff000), ephemeral=True)
         elif type == STATUS:
-            if guild_id not in WelcomeInfo.value:
+            if result is None:
                 await slash_tool.messages.mreply(interaction, "", embed=nextcord.Embed(title="ユーザー情報表示設定", description=f"サーバーで設定は`無効`です", color=0x00ff00), ephemeral=True)
             else:
-                await slash_tool.messages.mreply(interaction, "", embed=nextcord.Embed(title="ユーザー情報表示設定", description=f"サーバーで設定は`有効`です\n設定チャンネル: <#{WelcomeInfo.value[guild_id]}>", color=0x00ff00), ephemeral=True)
-            return
-        await database.default_push(client, WelcomeInfo)
+                await slash_tool.messages.mreply(interaction, "", embed=nextcord.Embed(title="ユーザー情報表示設定", description=f"サーバーで設定は`有効`です\n設定チャンネル: <#{result['channel_id']}>", color=0x00ff00), ephemeral=True)
 
 
     @nextcord.slash_command(name="ui", description="Send a message when user join/leave", guild_ids=n_fc.GUILD_IDS)
@@ -296,68 +245,68 @@ class user(commands.Cog):
         pass
 
 
+    @application_checks.has_permissions(manage_guild=True)
     @ui_slash.subcommand(name="set", description="Set channel of sent user info", description_localizations={nextcord.Locale.ja: "ユーザー表示チャンネルをセットします"})
     async def set_slash(
-        self,
-        interaction: Interaction,
-        channel: nextcord.abc.GuildChannel = SlashOption(
-            name="channel",
-            description="Channel of sent user info",
-            description_localizations={nextcord.Locale.ja: "メッセージを送信するチャンネルです"},
-            required=True
-        )
-    ):
-        if admin_check.admin_check(interaction.guild, interaction.user):
-            await self.ui_config(self.bot, self.bot.client, interaction, SET, interaction.guild.id, channel)
-        else:
-            await interaction.response.send_message("管理者権限がありません。", ephemeral=True)
+            self,
+            interaction: Interaction,
+            channel: nextcord.abc.GuildChannel = SlashOption(
+                name="channel",
+                description="Channel of sent user info",
+                description_localizations={nextcord.Locale.ja: "メッセージを送信するチャンネルです"},
+                required=True
+            )
+        ):
+        await self.ui_config(interaction, SET, interaction.guild.id, channel)
 
 
+    @application_checks.has_permissions(manage_guild=True)
     @ui_slash.subcommand(name="del", description="Delete channel of sent user info", description_localizations={nextcord.Locale.ja: "ユーザー表示チャンネル設定を削除します"})
     async def del_slash(
-        self,
-        interaction: Interaction
-    ):
-        if admin_check.admin_check(interaction.guild, interaction.user):
-            await self.ui_config(self.bot, self.bot.client, interaction, DEL, interaction.guild.id, None)
-        else:
-            await interaction.response.send_message("管理者権限がありません。", ephemeral=True)
+            self,
+            interaction: Interaction
+        ):
+        await self.ui_config(interaction, DEL, interaction.guild.id, None)
 
 
+    @application_checks.has_permissions(manage_guild=True)
     @ui_slash.subcommand(name="status", description="Check channel of sent user info", description_localizations={nextcord.Locale.ja: "ユーザー表示チャンネル設定を確認します"})
     async def status_slash(
-        self,
-        interaction: Interaction
-    ):
-        if admin_check.admin_check(interaction.guild, interaction.user):
-            await self.ui_config(self.bot, self.bot.client, interaction, STATUS, interaction.guild.id, None)
-        else:
-            await interaction.response.send_message("管理者権限がありません。", ephemeral=True)
+            self,
+            interaction: Interaction
+        ):
+        await self.ui_config(interaction, STATUS, interaction.guild.id, None)
 
 
+    @commands.has_permissions(manage_guild=True)
     @commands.command(name="ui", help="""\
 誰かがDiscordサーバーに加入/離脱した際に、指定したチャンネルにそのユーザーの情報を表示します。
 それだけです。
 
-`n!ui set [チャンネルID]`
+`n!ui set [チャンネル]`
 `n!ui del`""")
-    async def ui(self, ctx: commands.Context):
-        if admin_check.admin_check(ctx.guild, ctx.author):
-            mes_arg = ctx.message.content.split(" ")
-            if len(mes_arg) == 1:
-                await self.ui_config(self.bot, self.bot.client, ctx, STATUS, ctx.guild.id, None)
-            elif mes_arg[1] == "set":
-                set_id = int("".join(re.findall(r'\d', mes_arg[2])))
-                channel = await self.bot.fetch_channel(set_id)
-                await self.ui_config(self.bot, self.bot.client, ctx, SET, ctx.guild.id, channel)
-            elif mes_arg[1] == "del":
-                await self.ui_config(self.bot, self.bot.client, ctx, DEL, ctx.guild.id, None)
-            else:
-                await self.ui_config(self.bot, self.bot.client, ctx, STATUS, ctx.guild.id, None)
+    async def ui(self, ctx: commands.Context, setting: str | None = None, channel: str | int | None = None):
+        if setting is None:
+            await self.ui_config(ctx, STATUS, ctx.guild.id, None)
+        elif setting == "set":
+            CHANNEL = None
+            try:
+                CHANNEL = await self.bot.fetch_channel(int(channel))
+            except Exception:
+                for c in ctx.guild.channels:
+                    if c.name == channel:
+                        CHANNEL = c
+                        break
+            if CHANNEL is None:
+                await ctx.reply(f"指定されたンネル`{channel}`が見つかりませんでした。")
+                return
+            await self.ui_config(ctx, SET, ctx.guild.id, CHANNEL)
+        elif setting == "del":
+            await self.ui_config(ctx, DEL, ctx.guild.id, None)
         else:
-            await ctx.reply(embed=nextcord.Embed(title="Error", description=f"管理者権限がありません。", color=0xff0000))
+            await self.ui_config(ctx, STATUS, ctx.guild.id, None)
 
 
 
 def setup(bot, **kwargs):
-    bot.add_cog(user(bot, **kwargs))
+    bot.add_cog(User(bot, **kwargs))
