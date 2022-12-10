dba079640b011d01001dd2428ebccfd7f9273892
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2022-12-10 18:52:43 +0900

    update: user_joinをMongoDBに置き換え

--- cogs/user_join.py
+++ cogs/user_join.py
@@ -11,6 +11,8 @@ import HTTP_db
 import nextcord
 from nextcord.ext import commands
 
+from motor import motor_asyncio
+
 from util import admin_check, n_fc, eh, database, dict_list, parallel
 from util.nira import NIRA
 
@@ -18,70 +20,55 @@ from util.nira import NIRA
 # ユーザー参加時の挙動
 
 
-class RoleKeeper:
-    name = "role_keeper"
-    value = {}
-    default = {}
-    value_type = database.CHANNEL_VALUE
-
-
-class WelcomeInfo:
-    name = "welcome_info"
-    value = {}
-    default = {}
-    value_type = database.GUILD_VALUE
-
-
-class InviteData:
-    name = "invite_data"
-    value = {}
-    default = {}
-    value_type = database.CHANNEL_VALUE
-
-
-class user_join(commands.Cog):
+class UserJoin(commands.Cog):
     def __init__(self, bot: NIRA, **kwargs):
         self.bot = bot
+        self.winfo_collection: motor_asyncio.AsyncIOMotorCollection = self.bot.database["welcome_info"]
+        self.rk_collection: motor_asyncio.AsyncIOMotorCollection = self.bot.database["role_keeper"]
+        self.invite_collection: motor_asyncio.AsyncIOMotorCollection = self.bot.database["invite_data"]
         asyncio.ensure_future(self.fetch_role_keeper())
 
     async def fetch_role_keeper(self):
         await self.bot.wait_until_ready()
-        await database.default_pull(self.bot.client, RoleKeeper)
 
-        for i in self.bot.guilds:
-            if i.id not in RoleKeeper.value:
-                RoleKeeper.value[i.id] = {"rk": 0}
-            for j in i.members:
-                RoleKeeper.value[i.id][j.id] = [k.id for k in j.roles if k.id != i.id]
-
-        await database.default_push(self.bot.client, RoleKeeper)
+        for guild in self.bot.guilds:
+            rolekeeper = await self.rk_collection.find_one({"guild_id": guild.id})
+            if guild.id not in rolekeeper:
+                rolekeeper = {"rk": 0}
+            for member in guild.members:
+                rolekeeper[str(member.id)] = [role.id for role in member.roles if role.id != guild.id]
+            asyncio.ensure_future(self.rk_collection.update_one({"guild_id": guild.id}, {"$set": rolekeeper}, upsert=True))
 
     @commands.Cog.listener()
     async def on_member_join(self, member: nextcord.Member):
-        tasks = []
-        tasks.append(asyncio.ensure_future(database.default_pull(self.bot.client, WelcomeInfo)))
-        tasks.append(asyncio.ensure_future(database.default_pull(self.bot.client, RoleKeeper)))
-        tasks.append(asyncio.ensure_future(database.default_pull(self.bot.client, InviteData)))
-        await parallel.wait(tasks)
+        welcomeinfo = await self.winfo_collection.find_one({"guild_id": member.guild.id})
+        rolekeeper = await self.rk_collection.find_one({"guild_id": member.guild.id})
+        invites = await self.invite_collection.find_one({"guild_id": member.guild.id})
+
+        if welcomeinfo is None:
+            channel = None
+
         try:
-            channel = member.guild.get_channel(WelcomeInfo.value[member.guild.id])
-            if member.guild.id not in RoleKeeper.value:
-                RoleKeeper.value[member.guild.id] = {"rk": 0}
+            channel = member.guild.get_channel(welcomeinfo["channel_id"])
+            if rolekeeper is None:
+                rolekeeper = {"rk": False}
                 for i in range(len(member.guild.members)):
                     if member.guild.members[i].id != member.id:
-                        RoleKeeper.value[member.guild.id][member.guild.members[i].id] = [j.id for j in member.guild.members[i].roles if j.id != member.guild.id]
-                asyncio.ensure_future(database.default_push(self.bot.client, WelcomeInfo))
+                        rolekeeper[member.guild.members[i].id] = [j.id for j in member.guild.members[i].roles if j.id != member.guild.id]
+                asyncio.ensure_future(self.rk_collection.update_one({"guild_id": member.guild.id}, {"$set": rolekeeper}, upsert=True))
         except Exception as err:
             logging.error(err, traceback.format_exc())
 
         try:
-            if member.id not in RoleKeeper.value[member.guild.id]:
+            if member.id not in rolekeeper:
+                # ロールキーパーデータにない場合
                 embed = nextcord.Embed(
                     title="こんにちは！",
                     description=f"名前： `{member.name}`\nID: `{member.id}`",
                     color=0x00ff00
                 )
             else:
+                # ロールキーパーデータにある場合
                 embed = nextcord.Embed(
                     title="あれ、また会った？",
                     description=f"名前： `{member.name}`\nID: `{member.id}`",
@@ -90,63 +77,69 @@ class user_join(commands.Cog):
 
             if member.avatar is not None:
                 embed.set_thumbnail(url=member.avatar.url)
-            embed.add_field(name="アカウント製作日",
-                            value=f"```{member.created_at}```")
-            embed.add_field(name="現在のユーザー数",
-                            value=f"`{len(member.guild.members)}`人")
+
+            embed.add_field(
+                name="アカウント製作日",
+                value=f"```{member.created_at}```"
+            )
+            embed.add_field(
+                name="現在のユーザー数",
+                value=f"`{len(member.guild.members)}`人"
+            )
         except Exception as err:
             logging.error(err, traceback.format_exc())
 
         try:
             Invites = await member.guild.invites()
-            if member.guild.id not in InviteData.value:
-                InviteData.value[member.guild.id] = {i.url: [None, i.uses] for i in Invites}
+            if invites is None:
+                invites = {i.url: [None, i.uses] for i in Invites}
             else:
                 invitedUrl = None
-                for key, value in InviteData.value[member.guild.id].items():
+                for key, value in invites.items():
                     for i in Invites:
                         if i.url == key and i.uses != value[1]:
                             invitedUrl = i
-                            InviteData.value[member.guild.id][invitedUrl.url][1] = invitedUrl.uses
+                            invites[invitedUrl.url][1] = invitedUrl.uses
                             break
-                invitedFrom = f"[{InviteData.value[member.guild.id][invitedUrl.url][0]}]({invitedUrl.url})"
-                if InviteData.value[member.guild.id][invitedUrl.url][0] is None:
+                invitedFrom = f"[{invites[invitedUrl.url][0]}]({invitedUrl.url})"
+                if invites[invitedUrl.url][0] is None:
                     invitedFrom = f"[{invitedUrl.url}]({invitedUrl.url})"
                 embed.add_field(
-                    name="招待リンク", value=f"{invitedFrom}から招待を受けました！", inline=False)
-            asyncio.ensure_future(database.default_push(self.bot.client, InviteData))
+                    name="招待リンク",
+                    value=f"{invitedFrom}から招待を受けました！",
+                    inline=False
+                )
+            asyncio.ensure_future(self.invite_collection.update_one({"guild_id": member.guild.id}, {"$set": invites}, upsert=True))
+
         except Exception as err:
             logging.error(f"{err}\n{traceback.format_exc()}")
 
         try:
-            if member.guild.id in WelcomeInfo.value and channel is not None:
+            if channel is not None:
                 members_message = await channel.send(embed=embed)
             else:
                 members_message = None
 
         except Exception as err:
             logging.error(err)
-        await asyncio.sleep(2)
 
-        if RoleKeeper.value[member.guild.id]["rk"] == 1:
+        await asyncio.sleep(3)
 
+        if rolekeeper["rk"]:
             try:
-                if member.id not in RoleKeeper.value[member.guild.id]:
-                    RoleKeeper.value[member.guild.id][member.id] = [i.id for i in member.roles if i.id != member.guild.id]
-                    asyncio.ensure_future(database.default_push(self.bot.client, RoleKeeper))
-                else:
-                    for i in range(len(RoleKeeper.value[member.guild.id][member.id])):
-                        role = member.guild.get_role(RoleKeeper.value[member.guild.id][member.id][i])
+                if member.id in rolekeeper:
+                    for i in range(len(rolekeeper[member.id])):
+                        role = member.guild.get_role(rolekeeper[member.id][i])
                         if role is not None: await member.add_roles(role)
 
                     embed.add_field(
                         name="付与済みロール",
-                        value=f"{' '.join([f'<@&{i}>' for i in RoleKeeper.value[member.guild.id][member.id]])}"
+                        value=f"{' '.join([f'<@&{i}>' for i in rolekeeper[member.id]])}"
                     )
-                    await members_message.edit(embed=embed)
+                    if members_message is not None: await members_message.edit(embed=embed)
 
             except Exception as err:
-                await members_message.edit(f"ロール付与時に何かしらのエラーが発生しました。\n何度も発生する場合はお問い合わせください。\n`{err}`", embed=embed)
+                if members_message is not None: await members_message.edit(f"ロール付与時に何かしらのエラーが発生しました。\n何度も発生する場合はお問い合わせください。\n`{err}`", embed=embed)
                 logging.error(err)
 
 
@@ -158,23 +151,28 @@ class user_join(commands.Cog):
         time_diff = time_diff.total_seconds()
         user_id = member.id
         try:
-            await database.default_pull(self.bot.client, WelcomeInfo)
+            welcomeinfo = await self.winfo_collection.find_one({"guild_id": member.guild.id})
+
             user = await self.bot.fetch_user(user_id)
-            if member.guild.id in WelcomeInfo.value:
-                channel = self.bot.get_channel(WelcomeInfo.value[member.guild.id])
+            if welcomeinfo is not None:
+                channel = self.bot.get_channel(welcomeinfo["channel_id"])
             else:
                 channel = None
+
             embed = nextcord.Embed(
-                title="また来てねー！",
-                description=f"名前：`{user.name}`\nID：`{user.id}`",
+                title="またどこかで！",
+                description=f"名前: `{user.name}`\nID: `{user.id}`",
                 color=0x00ff00
             )
+
             if user.avatar is not None:
                 embed.set_thumbnail(url=user.avatar.url)
+
             embed.add_field(
                 name="現在のユーザー数",
                 value=f"`{len(member.guild.members)}`人"
             )
+
             role_text = ""
             role_ids = []
             for i in range(len(member.roles)):
@@ -185,25 +183,24 @@ class user_join(commands.Cog):
                 embed.add_field(name="付与されていたロール", value=f"{role_text}")
             if time_diff <= 100:
                 embed.add_field(name="即抜けRTA記録", value=f"`{round(time_diff, 4)}`秒")
-            if channel is not None:
-                removed_message = await channel.send(embed=embed)
+            if channel is not None: removed_message = await channel.send(embed=embed)
 
             # After send...
 
-            await database.default_pull(self.bot.client, RoleKeeper)
-            if member.guild.id not in RoleKeeper.value:
-                RoleKeeper.value[member.guild.id] = {"rk": 0}
+            rolekeeper = await self.rk_collection.find_one({"guild_id": member.guild.id})
+
+            if rolekeeper is None:
+                rolekeeper = {"rk": 0}
                 return
-            RoleKeeper.value[member.guild.id][member.id] = role_ids
-            await database.default_push(self.bot.client, RoleKeeper)
+            rolekeeper[member.id] = role_ids
+            await self.rk_collection.update_one({"guild_id": member.guild.id}, {"$set": rolekeeper}, upsert=True)
             return
         except Exception as err:
-            if channel is not None:
-                await removed_message.edit(f"おっと...これは大変ですね...\nユーザー離脱時の処理時にエラーが発生しました。\n`{err}`", embed=embed)
+            if channel is not None: await removed_message.edit(f"おっと...これは大変ですね...\nユーザー離脱時の処理時にエラーが発生しました。\n`{err}`", embed=embed)
             logging.error(f"ユーザー離脱時の情報表示システムのエラー\n{err}\n`{traceback.format_exc()}`")
             return
 
 
 def setup(bot, **kwargs):
-    bot.add_cog(user_join(bot, **kwargs))
+    bot.add_cog(UserJoin(bot, **kwargs))
 
