00ce98aeb9fe2425c51859a193688dfda4f9cac5
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2022-12-10 22:40:39 +0900

    update: SiritoriをMongoDBに対応

--- cogs/siritori.py
+++ cogs/siritori.py
@@ -7,6 +7,8 @@ import HTTP_db
 import nextcord
 from nextcord.ext import commands
 
+from motor import motor_asyncio
+
 from util import eh, srtr, database
 from util.n_fc import GUILD_IDS
 from util.nira import NIRA
@@ -17,46 +19,45 @@ STOP = ["stop", "off", "end", "exit", "ストップ", "おわり"]
 
 # しりとり管理系
 
-class srtr_data:
-    name = "srtr_data"
-    value = {}
-    default = {}
-    value_type = database.CHANNEL_VALUE
 
 class Siritori(commands.Cog):
     def __init__(self, bot: NIRA, **kwargs):
         self.bot = bot
+        self.collection: motor_asyncio.AsyncIOMotorCollection = self.bot.database["siritori_game"]
 
     @nextcord.slash_command(name="srtr", description="Siritori", guild_ids=GUILD_IDS)
     async def srtr(self):
         pass
 
-    async def srtr_control(self, start: bool, guild, channel) -> nextcord.Embed:
-        await database.default_pull(self.bot.client, srtr_data)
+    async def srtr_control(self, start: bool, guild: nextcord.Guild, channel: nextcord.abc.GuildChannel) -> nextcord.Embed:
+        """Control Siritori game"""
+        srtr_data = await self.collection.find_one({"guild_id": guild.id})
 
         if start:
-            if guild.id not in srtr_data.value:
-                srtr_data.value[guild.id] = {}
-
-            if channel.id in srtr_data.value[guild.id]:
-                return nextcord.Embed(title="しりとり", description=f"{channel.name}でしりとりは既に実行されています。", color=0x00ff00)
+            # Start Siritori game
+            if srtr_data is not None:
+                if channel.id in srtr_data["channels"]:
+                    return nextcord.Embed(title="しりとり", description=f"{channel.name}でしりとりは既に実行されています。", color=0x00ff00)
+                else:
+                    srtr_data["channels"].append(channel.id)
+            else:
+                srtr_data = {"guild_id": guild.id, "channels": [channel.id] }
 
             try:
-                srtr_data.value[guild.id][channel.id] = 1
-                await database.default_push(self.bot.client, srtr_data)
-
+                await self.collection.update_one({"guild_id": guild.id}, {"$set": srtr_data}, upsert=True)
             except Exception as err:
                 return eh(self.bot.client, err)
 
             return nextcord.Embed(title="しりとり", description=f"{channel.name}でしりとりを始めます。", color=0x00ff00)
 
         else:
-            if guild.id not in srtr_data.value or channel.id not in srtr_data.value[guild.id]:
+            # Stop Siritori game
+            if srtr_data is None or channel.id not in srtr_data["channels"]:
                 return nextcord.Embed(title="しりとり", description=f"{channel.name}でしりとりは実行されていません。", color=0x00ff00)
 
             try:
-                del srtr_data.value[guild.id][channel.id]
-                await database.default_push(self.bot.client, srtr_data)
+                srtr_data["channels"].remove(channel.id)
+                asyncio.ensure_future(self.collection.update_one({"guild_id": guild.id}, {"$set": srtr_data}, upsert=True))
 
             except Exception as err:
                 return eh(self.bot.client, err)
@@ -73,23 +74,12 @@ class Siritori(commands.Cog):
 
 
     """, aliases=["しりとり", "siritori"])
-    async def srtr_ctx(self, ctx: commands.Context):
-        if len(ctx.message.content.split(" ", 1)) == 1:
-            embed = nextcord.Embed(
-                title="しりとり",
-                description=f"`{self.bot.command_prefix}srtr start`でそのチャンネルでしりとり（風対話）を実行し、`{self.bot.command_prefix}srtr stop`でしりとりを停止します。",
-                color=0x00ff00
-            )
-            await ctx.reply(embed=embed)
-            return
-
-        control_arg = ctx.message.content.split(" ", 1)[1]
-
-        if control_arg in START:
+    async def srtr_ctx(self, ctx: commands.Context, control: str | None = None):
+        if control in START:
             embed = await self.srtr_control(
                 True, ctx.guild, ctx.channel)
 
-        elif control_arg in STOP:
+        elif control in STOP:
             embed = await self.srtr_control(
                 False, ctx.guild, ctx.channel)
 
@@ -118,10 +108,10 @@ class Siritori(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message: nextcord.Message):
         if not isinstance(message.channel, nextcord.DMChannel):
-            await database.default_pull(self.bot.client, srtr_data)
-            if message.guild.id in srtr_data.value:
-                if message.channel.id in srtr_data.value[message.guild.id]:
-                    await srtr.on_srtr(message, self.bot.client)
+            srtr_data = await self.collection.find_one({"guild_id": message.guild.id})
+            if srtr_data is not None:
+                if message.channel.id in srtr_data[message.guild.id]["value"]:
+                    await srtr.on_srtr(message, self.collection)
 
 
 
--- util/srtr.py
+++ util/srtr.py
@@ -6,11 +6,14 @@ import re
 import sys
 import traceback
 from os import getenv
+import asyncio
 
 import HTTP_db
 import jaconv
 import nextcord
 import pykakasi
+
+from motor import motor_asyncio
 
 from util import word_data, database
 
@@ -26,13 +29,8 @@ SYSDIR = sys.path[0]
 
 kks = pykakasi.kakasi()
 
-class srtr_bool:
-    name = "srtr_data"
-    value = {}
-    default = {}
-    value_type = database.CHANNEL_VALUE
-
-async def on_srtr(message: nextcord.Message, client: HTTP_db.Client):
+async def on_srtr(message: nextcord.Message, collection: motor_asyncio.AsyncIOMotorCollection):
+    """Process Siritori game"""
     if message.guild.id not in srtr_data:
         srtr_data[message.guild.id] = copy.deepcopy(word_data.words)
     try:
@@ -63,10 +61,10 @@ async def on_srtr(message: nextcord.Message, client: HTTP_db.Client):
         if lstr in word_data.chara_ary:
             if len(srtr_data[message.guild.id][f"{lstr}_wd"]) == 0:
                 await message.reply(embed=nextcord.Embed(title="しりとり", description=f"にらBOTには`{lstr}`から始まる言葉がない！\nあなたの勝ちです！\n再度しりとりをするには、`n!srtr start`と入力してください！", color=0x00ff00))
-                await database.default_pull(client, srtr_bool)
-                del srtr_bool.value[message.guild.id][message.channel.id]
-                del srtr_data[message.guild.id]
-                await database.default_push(client, srtr_bool)
+                # 事後処理
+                srtr_controller = await collection.find_one({"guild_id": message.guild.id})
+                srtr_controller["channels"].remove(message.channel.id)
+                await collection.update_one({"guild_id": message.guild.id}, {"$set": srtr_controller})
                 return
             w_rn = []
             while True:
@@ -87,10 +85,9 @@ async def on_srtr(message: nextcord.Message, client: HTTP_db.Client):
                     "[^A-Za-z\u3041-\u3096\u30A1-\u30FA\u4E00-\u9FFF\uF900-\uFA6D\uFF66-\uFF9D]+", "", srtr_str)[int(f"-{i+2}")]
                 if len(srtr_data[message.guild.id][f"{lstr}_wd"]) == 0:
                     await message.reply(embed=nextcord.Embed(title="しりとり", description=f"にらBOTには`{lstr}`から始まる言葉がない！\nあなたの勝ちです！\n再度しりとりをするには、`n!srtr start`と入力してください！", color=0x00ff00))
-                    await database.default_pull(client, srtr_bool)
-                    del srtr_bool.value[message.guild.id][message.channel.id]
-                    del srtr_data[message.guild.id]
-                    await database.default_push(client, srtr_bool)
+                    srtr_controller = await collection.find_one({"guild_id": message.guild.id})
+                    srtr_controller["channels"].remove(message.channel.id)
+                    await collection.update_one({"guild_id": message.guild.id}, {"$set": srtr_controller})
                     return
                 ply_dt = srtr_data[message.guild.id][f"{lstr}_wd"]
                 for _ in range(len(ply_dt)):
@@ -104,6 +101,6 @@ async def on_srtr(message: nextcord.Message, client: HTTP_db.Client):
                     else:
                         continue
             return
-    except Exception as err:
+    except Exception:
         await message.reply(f"エラーが発生しました。\n```sh\n{traceback.format_exc()}```")
         logging.error("Error", exc_info=True)
