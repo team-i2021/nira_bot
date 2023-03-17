4493d59b061ddc2a41f277253268bc70a84097c1
CrystalAlpha358 <qchigu76@gmail.com>
2023-03-17 20:22:58 +0900

    change: dropped HTTP_db and moved to MongoDB

--- cogs/pin.py
+++ cogs/pin.py
@@ -1,30 +1,38 @@
 import asyncio
+import dataclasses
 import logging
 from collections.abc import Callable, Coroutine
 from enum import Enum, auto
-from typing import Any
+from typing import Any, AsyncGenerator, TypeGuard
+from typing_extensions import Self
 
-import HTTP_db
 import nextcord
 from nextcord import Interaction
 from nextcord.ext import application_checks, commands
 
-from util import admin_check, n_fc, database
+from util import admin_check, n_fc
 from util.nira import NIRA
 
 # ボトムアップ的な機能
 
+COLLECTION_NAME = 'bottom_up'
+
 FIRST_SLEEP = 3
 SECOND_SLEEP = 2
 SLEEP_COUNT = 2
+
 MAX_LENGTH = 2000
+
+AsyncIOMotorCollection = Any
 
 glock = asyncio.Lock()
 
 
 # typing には交差型が無いなんて...
 class MessageableGuildChannel(nextcord.abc.Messageable, nextcord.abc.GuildChannel):
-    pass
+    @classmethod
+    def isinstance(cls, instance: Any) -> TypeGuard[Self]:
+        return all(isinstance(instance, c) for c in cls.__bases__)
 
 
 class Mode(Enum):
@@ -32,11 +40,137 @@ class Mode(Enum):
     OFF = auto()
 
 
-class PinData:
-    name = "bottom_up"
-    value = {}
-    default = {}
-    value_type = database.CHANNEL_VALUE
+@dataclasses.dataclass
+class _StoredPinDocument:
+    _id: int  # to store channel id; it is unique
+    text: str
+    last_message_id: int | None = None
+
+    @classmethod
+    def bind(cls, document: dict | None) -> Self | None:
+        if document is None:
+            return None
+        # TODO: 型チェック…？ 自前で実装したくはないから Pydantic あたりを使いたいが…
+        # データベース周りだから念の為にもやっておきたいなぁ…
+        return cls(**document)
+
+
+class StoredPin:
+    def __init__(
+            self,
+            collection: AsyncIOMotorCollection,
+            channel: MessageableGuildChannel,
+            text: str,
+            last_message: nextcord.Message | None = None,
+    ) -> None:
+        self._collection = collection
+        self._channel = channel
+        self._text = text
+        self._last_message = last_message
+
+    @property
+    def collection(self) -> AsyncIOMotorCollection:
+        return self._collection
+
+    @property
+    def channel(self) -> MessageableGuildChannel:
+        return self._channel
+
+    @property
+    def text(self) -> str:
+        return self._text
+
+    @text.setter
+    def text_setter(self, text: str) -> None:
+        self.set_text(text)
+
+    @property
+    def last_message(self) -> nextcord.Message | None:
+        return self._last_message
+
+    @last_message.setter
+    def last_message_setter(self, message: nextcord.Message | None) -> None:
+        self.set_last_message(message)
+
+    def set_text(self, text: str) -> Self:
+        self._text = text
+        return self
+
+    def set_last_message(self, message: nextcord.Message | None) -> Self:
+        self._last_message = message
+        return self
+
+    async def update(self) -> Self:
+        doc = _StoredPinDocument(
+            _id=self._channel.id,
+            text=self._text,
+            last_message_id=getattr(self._last_message, 'id', None),
+        )
+
+        await self._collection.replace_one({'_id': doc._id}, dataclasses.asdict(doc), upsert=True)
+        return self
+
+    async def clear(self) -> Self:
+        self._last_message = None
+        if self._channel is not None:
+            await self._collection.delete_one({'_id': self._channel.id})
+
+        return self
+
+
+class StoredPinCollection:
+    def __init__(self, bot: NIRA) -> None:
+        self._bot = bot
+        self._collection: AsyncIOMotorCollection = bot.database[COLLECTION_NAME]
+
+    def new(self, channel: MessageableGuildChannel, text: str) -> StoredPin:
+        return StoredPin(self._collection, channel, text, None)
+
+    async def get(self, channel: MessageableGuildChannel) -> StoredPin | None:
+        doc = _StoredPinDocument.bind(
+            await self._collection.find_one({'_id': channel.id}),
+        )
+        if doc is None:
+            return None
+
+        return await self._doc_to_storedpin(channel, doc)
+
+    async def get_all(self) -> AsyncGenerator[StoredPin | Exception, None]:
+        async for doc_raw in self._collection.find({'_id': {'$type': ['int', 'long']}}):
+            doc = _StoredPinDocument.bind(doc_raw)
+            assert doc is not None
+
+            channel = self._bot.get_channel(doc._id)
+            if channel is None or isinstance(channel, nextcord.PartialMessageable):
+                try:
+                    channel = await self._bot.fetch_channel(doc._id)
+                except (nextcord.NotFound, nextcord.Forbidden):
+                    continue
+                except Exception as e:
+                    yield e
+                    continue
+
+            if not MessageableGuildChannel.isinstance(channel):
+                continue
+
+            yield await self._doc_to_storedpin(channel, doc)
+
+    async def _doc_to_storedpin(
+            self,
+            channel: MessageableGuildChannel,
+            doc: _StoredPinDocument,
+    ) -> StoredPin:
+        text = doc.text
+        last_message: nextcord.Message | None = None
+        if doc.last_message_id is not None:
+            try:
+                msg = await channel.fetch_message(doc.last_message_id)
+            except (nextcord.NotFound, nextcord.Forbidden):
+                pass
+            else:
+                last_message = msg
+
+        return StoredPin(self._collection, channel, text, last_message)
 
 
 class PinLock:
@@ -79,33 +213,6 @@ class PinLock:
         self.callback = (func, args)
 
 
-async def update(
-        client: HTTP_db.Client,
-        channel: MessageableGuildChannel,
-        data: list[str, int | None] | None,
-) -> None:
-    guild = channel.guild
-
-    if data is None:
-        try:
-            del PinData.value[guild.id][channel.id]
-            if not PinData.value[guild.id]:
-                del PinData.value[guild.id]
-        except KeyError:
-            pass
-    else:
-        if guild.id not in PinData.value:
-            PinData.value[guild.id] = {}
-        PinData.value[guild.id][channel.id] = data
-
-    await database.default_push(client, PinData)
-
-
-async def pull_data(client: HTTP_db.Client) -> None:
-    async with glock:
-        await database.default_pull(client, PinData)
-
-
 def err_embed(description: str) -> nextcord.Embed:
     return nextcord.Embed(title="エラー", description=description, color=0xff0000)
 
@@ -113,8 +220,8 @@ def err_embed(description: str) -> nextcord.Embed:
 class BottomUp(commands.Cog):
     def __init__(self, bot: NIRA):
         self.bot = bot
+        self.collection = StoredPinCollection(bot)
         self._locks: dict[int, PinLock] = {}
-        asyncio.ensure_future(pull_data(bot.client))
 
     async def _pin(
             self,
@@ -135,7 +242,7 @@ class BottomUp(commands.Cog):
                     return (None, err_embed(f"文字数は{MAX_LENGTH}文字以下である必要があります。"))
 
                 async with self._get_lock(channel.id).save:
-                    await update(self.bot.client, channel, [message, None])
+                    await self.collection.new(channel, message).update()
 
                 return (
                     "メッセージを設定しました。",
@@ -143,21 +250,22 @@ class BottomUp(commands.Cog):
                 )
 
             case Mode.OFF:
-                if guild.id not in PinData.value or channel.id not in PinData.value[guild.id]:
-                    return (None, err_embed("このチャンネルにはメッセージが設定されていません。"))
-
                 lock = self._get_lock(channel.id)
                 async with lock.save:
-                    last_id = PinData.value[guild.id][channel.id][1]
+                    store = await self.collection.get(channel)
+                    if store is None:
+                        return (None, err_embed("このチャンネルにはメッセージが設定されていません。"))
+
+                    last_msg = store.last_message
                     failed = False
-                    await update(self.bot.client, channel, None)
-                    if last_id is not None:
+                    if last_msg is not None:
                         try:
-                            await (await channel.fetch_message(last_id)).delete()
+                            await last_msg.delete()
                         except nextcord.NotFound:
                             pass
                         except nextcord.Forbidden:
                             failed = True
+                    await store.clear()
                     lock.sleep_unlock()
                     del self._locks[channel.id]
 
@@ -221,31 +329,33 @@ offにするには、`n!pin off`と送信してください。
             ctx: commands.Context,
             ch_id: int | None = None,
     ) -> None:
-        ch: nextcord.abc.GuildChannel | None = None
-        if ch_id is not None:
-            if (ch := self.bot.get_channel(ch_id)) is None:
-                try:
-                    ch = await self.bot.fetch_channel(ch_id)
-                except nextcord.NotFound:
-                    await ctx.reply("Channel not found")
-                    return
-                except nextcord.Forbidden:
-                    await ctx.reply("Permission denied")
-                    return
-
-        if isinstance(ch, nextcord.abc.Messageable):
-            if ch.guild.id in PinData.value and ch.id in PinData.value[ch.guild.id]:
-                msg = await ctx.reply(f"Refreshing {ch.id}...")
-                await self._refresh_channel(ch)
-                await msg.edit("Refresh finished.")
-            else:
-                await ctx.reply("Not configured")
-        elif ch:
-            await ctx.reply("Not messageable")
-        else:
+        if ch_id is None:
             msg = await ctx.reply("Refreshing...")
             await self._refresh_messages()
-            await msg.edit("Refresh finished.")
+            await msg.edit(content="Refresh finished.")
+            return
+
+        channel = self.bot.get_channel(ch_id)
+        if channel is None:
+            try:
+                channel = await self.bot.fetch_channel(ch_id)
+            except nextcord.NotFound:
+                await ctx.reply("Channel not found")
+                return
+            except nextcord.Forbidden:
+                await ctx.reply("Permission denied")
+                return
+
+        if not MessageableGuildChannel.isinstance(channel):
+            await ctx.reply("Not messageable guild channel")
+            return
+        elif await self.collection.get(channel) is None:
+            await ctx.reply("Not configured")
+            return
+
+        msg = await ctx.reply(f"Refreshing {ch_id}...")
+        await self._refresh_channel(channel)
+        await msg.edit(content="Refresh finished.")
 
     @pin_debug_c.command(name="lock")
     @commands.is_owner()
@@ -322,40 +432,35 @@ offにするには、`n!pin off`と送信してください。
             self._locks[ch_id] = PinLock()
         return self._locks[ch_id]
 
-    async def _send_msg(self, ch: MessageableGuildChannel) -> bool:
-        msg = PinData.value[ch.guild.id][ch.id][0]
-        msg_id = None
+    async def _send_msg(self, store: StoredPin) -> bool:
         try:
-            msg_id = (await ch.send(msg)).id
-        except nextcord.Forbidden:
-            return False
-        except Exception:
-            logging.exception("Error while sending message")
+            msg = await store.channel.send(store.text)
+        except Exception as e:
+            if isinstance(e, nextcord.Forbidden):
+                logging.exception("Error while sending message")
             return False
 
         try:
-            await update(self.bot.client, ch, [msg, msg_id])
+            await store.set_last_message(msg).update()
         except Exception:
             logging.exception("Error while updating database")
 
         return True
 
-    async def _del_msg(self, ch: MessageableGuildChannel) -> bool:
-        data = PinData.value[ch.guild.id][ch.id]
-        if data[1] is None:
+    async def _del_msg(self, store: StoredPin) -> bool:
+        if store.last_message is None:
             return True
 
         try:
-            await (await ch.fetch_message(data[1])).delete()
+            await store.last_message.delete()
         except (nextcord.NotFound, nextcord.Forbidden):
             pass
         except Exception:
             logging.exception("Error while deleting message")
             return False
 
-        data[1] = None
         try:
-            await update(self.bot.client, ch, data)
+            await store.set_last_message(None).update()
         except Exception:
             logging.exception("Error while updating database")
 
@@ -364,9 +469,9 @@ offにするには、`n!pin off`と送信してください。
     async def _refresh_channel(self, ch: MessageableGuildChannel) -> None:
         lock = self._get_lock(ch.id)
         async with lock.save:
-            last_msg = None
+            last_message = None
             try:
-                last_msg = (await ch.history(limit=1).flatten())[0]
+                last_message = (await ch.history(limit=1).flatten())[0]
             except IndexError:
                 pass
             except nextcord.Forbidden:
@@ -378,26 +483,21 @@ offにするには、`n!pin off`と送信してください。
             if lock.sleep.locked():
                 return
 
-            last_id = PinData.value[ch.guild.id][ch.id][1]
-            if last_msg is None or last_msg.id != last_id and await self._del_msg(ch):
-                await self._send_msg(ch)
+            store = await self.collection.get(ch)
+            if store is None:
+                return
+            elif (last_message is None
+                  or last_message != store.last_message and await self._del_msg(store)):
+                await self._send_msg(store)
                 await lock.sleep_lock(FIRST_SLEEP, SECOND_SLEEP)
 
     async def _refresh_messages(self) -> None:
         async with glock:
-            for _g in PinData.value.values():
-                for ch_id, data in _g.items():
-                    ch = self.bot.get_channel(ch_id)
-                    if ch is None:
-                        try:
-                            ch = await self.bot.fetch_channel(ch_id)
-                        except (nextcord.NotFound, nextcord.Forbidden):
-                            continue
-                        except Exception:
-                            logging.exception("Error while searching for a channel")
-                            continue
-
-                    await self._refresh_channel(ch)
+            async for store in self.collection.get_all():
+                if isinstance(store, StoredPin):
+                    await self._refresh_channel(store.channel)
+                else:
+                    logging.exception("Error while fetching channel", exc_info=store)
 
     @commands.Cog.listener()
     async def on_ready(self) -> None:
@@ -408,27 +508,28 @@ offにするには、`n!pin off`と送信してください。
         async with glock:
             pass
 
-        ch = message.channel
-        if (message.flags.ephemeral
-                or not hasattr(ch, "guild")
-                or message.guild.id not in PinData.value
-                or ch.id not in PinData.value[message.guild.id]):
+        channel = message.channel
+        if message.flags.ephemeral or not MessageableGuildChannel.isinstance(channel):
             return
 
-        lock = self._get_lock(ch.id)
+        lock = self._get_lock(channel.id)
         async with lock.save:
-            last_id = PinData.value[ch.guild.id][ch.id][1]
-            if message.id == last_id:
+            store = await self.collection.get(channel)
+            if store is None:
+                return
+
+            last_message = store.last_message
+            if message == last_message:
                 return
 
             lock.count += 1
             if lock.sleep.locked():
                 if not lock.callback:
-                    lock.set_callback(self._refresh_channel, ch)
+                    lock.set_callback(self._refresh_channel, channel)
                 return
 
-            if await self._del_msg(ch):
-                await self._send_msg(ch)
+            if await self._del_msg(store):
+                await self._send_msg(store)
                 await lock.sleep_lock(FIRST_SLEEP, SECOND_SLEEP)
 
 
