44ae06e5251ed844e239f67f01d12cbb3675b6f6
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2023-03-22 00:45:34 +0900

    enhance: add client session

--- util/nira.py
+++ util/nira.py
@@ -1,3 +1,5 @@
+import aiohttp
+
 import nextcord
 from nextcord.ext import commands
 
@@ -5,6 +7,8 @@ import motor
 from motor import motor_asyncio
 
 from util.n_fc import py_admin
+
+from typing import Any
 
 class NIRA(commands.Bot):
     """
@@ -15,15 +19,18 @@ class NIRA(commands.Bot):
     def __init__(
             self,
             debug: bool = False,
-            token: str = None,
+            token: str | None = None,
             client = None,
             mongo: motor.motor_asyncio.AsyncIOMotorClient = None,
             database_name: str = "nira-bot",
-            *args,
-            **kwargs
+            shard_id: int = 0,
+            shard_count: int = 1,
+            settings: dict[str, dict[str, str] | list[str | int] | str | int] | None = None,
+            *args: Any,
+            **kwargs: Any
         ):
         self.debug: bool = debug
-        self.__token: str = token
+        self.__token: str | None = token
         self.client = client # HTTP_dbとして呼び出す（今まで通りself.client + database.default_pushとかで呼び出す）
 
         self.__mongo: motor_asyncio.AsyncIOMotorClient = mongo # おいてるだけだから使わない
@@ -31,7 +38,11 @@ class NIRA(commands.Bot):
         self.database: motor_asyncio.AsyncIOMotorDatabase = self.__mongo[database_name] # MongoDBとして呼び出す（self.database["collection_name"]として呼び出す）
         self.database_name: str = database_name
 
-        self.ready_shard = []
+        self.shard_id = shard_id
+        self.shard_count = shard_count
+        self.settings = settings
+
+        self.session = aiohttp.ClientSession()
         #self.main_prefix: str = (lambda x: x[0] if type(x) in [list, tuple, set] else x)(**kwargs[""])
         return super().__init__(*args, **kwargs)
 
