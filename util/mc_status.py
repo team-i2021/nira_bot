bca31a34b7a0ca2601e166fc0d796478c8c6e166
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2023-03-28 21:31:30 +0900

    improve: コード改善

--- util/mc_status.py
+++ util/mc_status.py
@@ -1,9 +1,6 @@
-import asyncio
-import threading
-import mcstatus
-
 from mcstatus import JavaServer, BedrockServer
-
+from mcstatus.pinger import PingResponse
+from mcstatus.bedrock_status import BedrockStatusResponse
 JAVA = 1
 BE = 2
 
@@ -11,7 +8,7 @@ BE = 2
 # JavaEdition default port:25565
 # BedrockEdition default port:19132
 
-async def java_status(host: str, port: int) -> mcstatus.pinger.PingResponse:
+async def java_status(host: str, port: int) -> PingResponse | Exception:
     """Status checker of JAVA"""
     try:
         server = JavaServer(host=host, port=port)
@@ -20,7 +17,7 @@ async def java_status(host: str, port: int) -> mcstatus.pinger.PingResponse:
     except Exception as err:
         return err
 
-async def bedrock_status(host: str, port: int) -> mcstatus.bedrock_status.BedrockStatusResponse:
+async def bedrock_status(host: str, port: int) -> BedrockStatusResponse | Exception:
     """Status checker of BEDROCK"""
     try:
         server = BedrockServer(host=host, port=port)
