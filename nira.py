b66140188ec793f6fba3993461190464fb0ed44d
CrystalAlpha358 <qchigu76@gmail.com>
2023-04-04 17:19:18 +0900

    improve: minor code formatting

--- nira.py
+++ nira.py
@@ -1,14 +1,11 @@
 # 沢山のインポート
-import asyncio
 import json
 import logging
 import os
 import sys
 import traceback
-import time
 
 import nextcord
-
 from motor import motor_asyncio
 
 from util import n_fc, database
@@ -17,19 +14,23 @@ from util.nira import NIRA
 sys.setrecursionlimit(10000)  # エラー回避
 print("モジュールインポート完了")
 
-import onami
 
 # 設定読み込み
-if os.path.isfile(f'{sys.path[0]}/setting.json') == False:
-    print("""BOTの設定ファイルが見つかりませんでした。
+if not os.path.isfile(f"{sys.path[0]}/setting.json"):
+    print(
+        """\
+BOTの設定ファイルが見つかりませんでした。
 「nira.py」があるフォルダに「setting.json」をおいてください。
 「setting_temp.json」というテンプレートがありますのでそちらを参考にしてください。
 
-または、「setup.py」を実行して、画面の指示通りにしてください。""", file=sys.stderr)
+または、「setup.py」を実行して、画面の指示通りにしてください。
+""",
+        file=sys.stderr,
+    )
     os._exit(0)
 
 HOME = os.path.dirname(os.path.abspath(__file__))
-SETTING = json.load(open(f'{sys.path[0]}/setting.json', 'r'))
+SETTING = json.load(open(f"{sys.path[0]}/setting.json", "r"))
 
 
 n_fc.GUILD_IDS = SETTING["guild_ids"]
@@ -43,44 +44,32 @@ if "-d" in sys.argv:
     print(f"NIRA Bot Debug Mode\nThe following will be loaded... :{LOAD_COGS}")
 
 
-##### データベースの設定 #####
+# データベースの設定
 CLIENT = database.openClient()
 _MONGO_CLIENT = motor_asyncio.AsyncIOMotorClient(SETTING["database_url"])
 
 
-# for i in range(1, 4):
-#     print(f"データベース接続チェック {i}/3...")
-#     result = requests.post(f"{CLIENT.url}/auth", json={"password": CLIENT.password})
-#     if result.status_code != 200:
-#         if i <= 2: print(f"データベースに接続できませんでした。\nステータスコード: {result.status_code}");
-#         else: print("データベースに接続できませんでした。\nプログラムを終了します。"); os._exit(0)
-#     else:
-#         if result.json()["status"] == "success": print("データベースに接続できました。"); break
-#         else: print("データベースのパスワードが違います。\nプログラムを終了します。"); os._exit(0)
-#     time.sleep(1)
-
-
-##### BOTの設定 #####
-intents = nextcord.Intents.all() # 全部のインテントが有効になる
-intents.typing = False # 重くなる可能性があるのでTypingを無効化
-intents.presences = False # 未認証なのでPresence Intentは無効化
-intents.members = True # Members Intentを有効化
-intents.message_content = True # Message Content Intentを有効化
+# BOTの設定
+intents = nextcord.Intents.all()  # 全部のインテントが有効になる
+intents.typing = False  # 重くなる可能性があるのでTypingを無効化
+intents.presences = False  # 未認証なのでPresence Intentは無効化
+intents.members = True  # Members Intentを有効化
+intents.message_content = True  # Message Content Intentを有効化
 
 bot = NIRA(
-    command_prefix=SETTING["prefix"],
-    intents=intents,
-    help_command=None,
-    activity=nextcord.Game(name="Connecting...", type=1),
-    status=nextcord.Status.dnd,
+    client=CLIENT,  # http_db
+    mongo=_MONGO_CLIENT,  # mongo_db
     debug=DEBUG,
     token=SETTING["tokens"]["nira_bot"],
-    client=CLIENT, # http_db
-    mongo=_MONGO_CLIENT, # mongo_db
     database_name=SETTING["database_name"],
     shard_id=SETTING["shard_id"],
     shard_count=SETTING["shard_count"],
     settings=SETTING,
+    command_prefix=SETTING["prefix"],
+    intents=intents,
+    help_command=None,
+    status=nextcord.Status.dnd,
+    activity=nextcord.Game(name="Connecting...", type=1),
 )
 
 bot.load_extension("onami")
@@ -88,33 +77,42 @@ bot.load_extension("onami")
 print("BOTの設定完了")
 
 
-##### loggingの設定 #####
+# loggingの設定
 
-FORMATTER = '%(asctime)s$%(filename)s$%(lineno)d$%(funcName)s$%(levelname)s:%(message)s'
+FORMATTER = "%(asctime)s$%(filename)s$%(lineno)d$%(funcName)s$%(levelname)s:%(message)s"
 logging.basicConfig(
     format=FORMATTER,
-    filename=f'{sys.path[0]}/nira.log',
+    filename=f"{sys.path[0]}/nira.log",
     level=logging.INFO,
 )
 
 print("Logging設定完了")
 
 
-
 @bot.event
 async def on_ready():
     if not bot.debug:
-        await bot.change_presence(activity=nextcord.Game(name=f"{bot.command_prefix}help | にらゲー", type=1), status=nextcord.Status.online)
+        await bot.change_presence(
+            activity=nextcord.Game(name=f"{bot.command_prefix}help | にらゲー", type=1),
+            status=nextcord.Status.online,
+        )
     else:
-        await bot.change_presence(activity=nextcord.Game(name=f"{bot.command_prefix} | にらゲー開発", type=1), status=nextcord.Status.dnd)
+        await bot.change_presence(
+            activity=nextcord.Game(name=f"{bot.command_prefix} | にらゲー開発", type=1),
+            status=nextcord.Status.dnd,
+        )
+
+    assert bot.user
 
     print("Welcome to nira-bot!")
-    print(f"""\
+    print(
+        f"""\
 USER: {bot.user.name}#{bot.user.discriminator}
 ID: {bot.user.id}
 COGS: {[dict(bot.cogs)[i].qualified_name for i in dict(bot.cogs).keys()]}
 COMMANDS: {[i.name for i in list(bot.commands)]}
-""")
+"""
+    )
 
 
 # 暫定: 元のエラーハンドラが反応して標準エラーにスタックトレースを出力してしまうので
@@ -127,6 +125,7 @@ async def on_application_command_error(interaction, exception):
 if not bot.debug:
     bot.event(on_application_command_error)
 
+
 # load extensions
 cogs_dir = HOME + "/cogs"
 cogs_num = len(os.listdir(cogs_dir))
@@ -136,14 +135,18 @@ if bot.debug:
     cogs_list = LOAD_COGS
 for i in range(cogs_num):
     try:
-        if cogs_list[i][-3:] != ".py" or cogs_list[i] == "__pycache__" or cogs_list[i] == "not_ready.py" or cogs_list[i] in UNLOAD_COGS:
-            continue
-        bot.load_extension(f"cogs.{cogs_list[i][:-3]}")
-    except Exception as err:
+        if not (
+            cogs_list[i][-3:] != ".py"
+            or cogs_list[i] == "__pycache__"
+            or cogs_list[i] == "not_ready.py"
+            or cogs_list[i] in UNLOAD_COGS
+        ):
+            bot.load_extension(f"cogs.{cogs_list[i][:-3]}")
+    except Exception:
         print("Cog読み込み失敗:", cogs_list[i], file=sys.stderr)
         traceback.print_exc()
-        logging.error(f"Cog読み込み失敗: {cogs_list[i]}", exc_info=True)
-        continue
+        logging.exception(f"Cog読み込み失敗: {cogs_list[i]}")
+
 print("Cogの読み込み終了")
 
 
