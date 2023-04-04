6f79cd7eb452c783eefb49eed51bf559a570868f
CrystalAlpha358 <qchigu76@gmail.com>
2023-04-04 10:14:05 +0900

    improve: minor code formatting

--- util/nira.py
+++ util/nira.py
@@ -1,14 +1,14 @@
 import aiohttp
 
 import nextcord
+from HTTP_db import Client
+from motor.motor_asyncio import AsyncIOMotorClient
 from nextcord.ext import commands
-
-import motor
-from motor import motor_asyncio
 
 from util.n_fc import py_admin
 
 from typing import Any
+
 
 class NIRA(commands.Bot):
     """
@@ -16,34 +16,36 @@ class NIRA(commands.Bot):
 
     An object that extends `commands.Bot`.
     """
+
     def __init__(
-            self,
-            debug: bool = False,
-            token: str | None = None,
-            client = None,
-            mongo: motor.motor_asyncio.AsyncIOMotorClient = None,
-            database_name: str = "nira-bot",
-            shard_id: int = 0,
-            shard_count: int = 1,
-            settings: dict[str, dict[str, str] | list[str | int] | str | int] | None = None,
-            *args: Any,
-            **kwargs: Any
-        ):
-        self.debug: bool = debug
-        self.__token: str | None = token
-        self.client = client # HTTP_dbとして呼び出す（今まで通りself.client + database.default_pushとかで呼び出す）
+        self,
+        client: Client,
+        mongo: AsyncIOMotorClient,
+        debug: bool = False,
+        token: str | None = None,
+        database_name: str = "nira-bot",
+        shard_id: int = 0,
+        shard_count: int = 1,
+        settings: dict[str, dict[str, str] | list[str | int] | str | int] | None = None,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        self.debug = debug
+        self.__token = token
+        self.client = client  # HTTP_dbとして呼び出す（今まで通りself.client + database.default_pushとかで呼び出す）
 
-        self.__mongo: motor_asyncio.AsyncIOMotorClient = mongo # おいてるだけだから使わない
+        self.__mongo = mongo  # おいてるだけだから使わない
 
-        self.database: motor_asyncio.AsyncIOMotorDatabase = self.__mongo[database_name] # MongoDBとして呼び出す（self.database["collection_name"]として呼び出す）
-        self.database_name: str = database_name
+        # MongoDBとして呼び出す（self.database["collection_name"]として呼び出す）
+        self.database = self.__mongo[database_name]
+        self.database_name = database_name
 
         self.shard_id = shard_id
         self.shard_count = shard_count
         self.settings = settings
 
         self.session = aiohttp.ClientSession()
-        #self.main_prefix: str = (lambda x: x[0] if type(x) in [list, tuple, set] else x)(**kwargs[""])
+        # self.main_prefix: str = (lambda x: x[0] if type(x) in [list, tuple, set] else x)(**kwargs[""])
         return super().__init__(*args, **kwargs)
 
     def run(self) -> None:
