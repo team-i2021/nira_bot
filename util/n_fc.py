9cb2f2e465a39585be2b47a2d11a5ae0dd10aad3
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2023-04-09 16:41:48 +0900

    change: on/offエイリアスの型をsetに変更

--- util/n_fc.py
+++ util/n_fc.py
@@ -1,5 +1,5 @@
 py_admin: tuple[int, ...] = ()
 GUILD_IDS: tuple[int, ...] = ()
 
-on_ali: list[str] = ["1", "on", "On", "ON", "true", "True", "TRUE", "yes", "Yes", "YES"]
-off_ali: list[str] = ["0", "off", "Off", "OFF", "false", "False", "FALSE", "no", "No", "NO"]
+on_ali: set[str] = {"1", "on", "On", "ON", "true", "True", "TRUE", "yes", "Yes", "YES"}
+off_ali: set[str] = {"0", "off", "Off", "OFF", "false", "False", "FALSE", "no", "No", "NO"}
