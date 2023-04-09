f94e99c7bda3d40f1fae48ee1b5a75e311333d9d
nattyan-tv <73095116+nattyan-tv@users.noreply.github.com>
2023-04-09 18:08:04 +0900

    change: 知識アップデートに伴う一部の変更

--- util/genshin_token.js
+++ util/genshin_token.js
@@ -1,7 +1,7 @@
 const main = () => {
     const domain = (new URL(location.href)).hostname.replace("www.", "").replace("act.", "").replace("m.", "").replace("webstatic-sea.", "");
 
-    if (domain != "hoyolab.com") {
+    if (domain !== "hoyolab.com") {
         if(confirm("HoYoLABのページにアクセスし直してからこのスクリプトを実行してください。\nHoyoLABにアクセスしますか？\nスクリプトは実行し直す必要があります。")){
             location.href = "https://www.hoyolab.com/";
             return;
@@ -10,32 +10,34 @@ const main = () => {
     }
 
     const cookies = document.cookie.split("; ");
-    let ltuid = "";
-    let ltoken = "";
+    let ltuid = null;
+    let ltoken = null;
 
     cookies.forEach(function(cookie){
         const data = cookie.split("=");
-        if (data[0] == "ltuid"){
+        if (data[0] === "ltuid"){
             ltuid = data[1];
-        } else if (data[0] == "ltoken"){
+        } else if (data[0] === "ltoken"){
             ltoken = data[1];
         }
     });
 
-    if (ltuid == "") {
+    if (ltuid === null) {
         alert("ログインし直してください。\nHoYoLAB UIDが見つかりませんでした。");
         return;
-    } else if (ltoken == "") {
+    } else if (ltoken === null) {
         alert("ログインし直してください。\nログイントークンが見つかりませんでした。");
         return;
     }
 
+    const code = btoa(`${ltuid}/${ltoken}`)
+
     document.write(`
 <html>
-    <head><title>にらBOT</title></head>
+    <head><title>Genshin Connect</title></head>
     <body>
         <h1>下をコピー</h1>
-        <textarea id="target" style="width: 80%; height: 30%" readonly>` + ltuid + `/` + ltoken + `</textarea><br>
+        <textarea id="target" style="width: 80%; height: 30%" readonly>` + code + `</textarea><br>
         <button onclick="clipCopy()">コピー</button>
         <h1>絶対にこの画面のスクリーンショットなどを他の人に見せないで下さい。</h1>
         <div>HoyoLabに戻るにはページを更新するか<a href="https://www.hoyolab.com/">こちらからHoYoLABトップページに戻る</a></div>
@@ -54,8 +56,7 @@ const main = () => {
 main();
 
 /* iOS Share Extension用 終了処理 */
-try{
+
+if (typeof completion === "function") {
     completion();
-} catch {
-    console.log("Exit");
 }
