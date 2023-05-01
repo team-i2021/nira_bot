d1bd75c9ca1ea5d8732361f5453c32915aacf567
CrystalAlpha358 <qchigu76@gmail.com>
2023-05-02 00:46:08 +0900

    DEPRECATED: branch is discontined

--- util/n_fc.py
+++
@@ -1,5 +0,0 @@
-py_admin: tuple[int, ...] = ()
-GUILD_IDS: tuple[int, ...] = ()
-
-on_ali: set[str] = {"1", "on", "On", "ON", "true", "True", "TRUE", "yes", "Yes", "YES"}
-off_ali: set[str] = {"0", "off", "Off", "OFF", "false", "False", "FALSE", "no", "No", "NO"}
